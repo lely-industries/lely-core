4e85463069f9f50ed40f10473ae64582b02f0b6d
J. S. Seldenthuis <jseldenthuis@lely.com>
2016-03-15 17:09:46 +0100

    add red-black tree implementation

---
+++ include/lely/util/rbtree.h
@@ -0,0 +1,226 @@
+/*!\file
+ * This header file is part of the utilities library; it contains the
+ * <a href="http://en.wikipedia.org/wiki/Red-black_tree">red-black tree</a>
+ * declarations.
+ *
+ * A red-black tree is a type of self-balancing binary tree. This implementation
+ * is based on chapters 12 and 13 in: \n
+ * T. H. Cormen et al., <em>Introduction to Algorithms</em> (third edition), MIT Press (2009).
+ *
+ * The red-black tree implemented here is generic and can be used for any kind
+ * of key-value pair; only (void) pointers to keys are stored. Upon
+ * initialization of the tree, the user is responsible for providing a suitable
+ * comparison function (#cmp_t).
+ *
+ * \copyright 2016 Lely Industries N.V.
+ *
+ * \author J. S. Seldenthuis <jseldenthuis@lely.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef LELY_UTIL_RBTREE_H
+#define LELY_UTIL_RBTREE_H
+
+#include <lely/util/cmp.h>
+
+/*!
+ * A node in a red-black tree. To associate a value with a node, embed the node
+ * in a struct containing the value and use structof() to obtain the struct from
+ * the node.
+ *
+ * \see rbtree
+ */
+struct rbnode {
+	/*!
+	 * A pointer to the key of this node. The key MUST be set before the
+	 * node is inserted into a tree and MUST NOT be modified while the node
+	 * is part of the tree.
+	 */
+	const void *key;
+	/*!
+	 * A pointer to the parent node. The least significant bit contains the
+	 * color of this node (0 = black, 1 = red).
+	 */
+	uintptr_t parent;
+	//! A pointer to the left child node.
+	struct rbnode *left;
+	//! A pointer to the right child node.
+	struct rbnode *right;
+};
+
+//! A red-black tree.
+struct rbtree {
+	//! A pointer to the function used to compare two keys.
+	cmp_t *cmp;
+	//! A pointer to the root node of the tree.
+	struct rbnode *root;
+	//! The number of nodes stored in the tree.
+	size_t num_nodes;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*!
+ * Initializes a node in a red-black tree.
+ *
+ * \param node a pointer to the node to be initialized.
+ * \param key  a pointer to the key for this node. The key MUST NOT be modified
+ *             while the node is part of a tree.
+ */
+static inline void rbnode_init(struct rbnode *node, const void *key);
+
+/*!
+ * Returns a pointer to the previous (in-order) node in a red-black tree with
+ * respect to \a node. This is, at worst, an O(log(n)) operation.
+ *
+ * \see rbnode_next()
+ */
+LELY_UTIL_EXTERN struct rbnode *rbnode_prev(const struct rbnode *node);
+
+/*!
+ * Returns a pointer to the next (in-order) node in a red-black tree with
+ * respect to \a node. This is, at worst, an O(log(n)) operation. However,
+ * visiting all nodes in order is an O(n) operation, and therefore, on average,
+ * O(1) for each node.
+ *
+ * \see rbnode_prev()
+ */
+LELY_UTIL_EXTERN struct rbnode *rbnode_next(const struct rbnode *node);
+
+/*!
+ * Initializes a red-black tree.
+ *
+ * \param tree a pointer to the tree to be initialized.
+ * \param cmp  a pointer to the function used to compare two keys.
+ */
+static inline void rbtree_init(struct rbtree *tree, cmp_t *cmp);
+
+//! Returns 1 if the red-black tree is empty, and 0 if not.
+static inline int rbtree_empty(const struct rbtree *tree);
+
+/*!
+ * Returns the size (in number of nodes) of a red-black tree. This is an O(1)
+ * operation.
+ */
+static inline size_t rbtree_size(const struct rbtree *tree);
+
+/*!
+ * Inserts a node into a red-black tree. This is an O(log(n)) operation. This
+ * function does not check whether a node with the same key already exists, or
+ * whether the node is already part of another tree.
+ *
+ * \see rbtree_remove(), rbtree_find()
+ */
+LELY_UTIL_EXTERN void rbtree_insert(struct rbtree *tree, struct rbnode *node);
+
+/*!
+ * Removes a node from a red-black tree. This is an O(log(n)) operation.
+ *
+ * \see rbtree_insert()
+ */
+LELY_UTIL_EXTERN void rbtree_remove(struct rbtree *tree, struct rbnode *node);
+
+/*!
+ * Finds a node in a red-black tree. This is an O(log(n)) operation.
+ *
+ * \returns a pointer to the node if found, or NULL if not.
+ *
+ * \see rbtree_insert()
+ */
+LELY_UTIL_EXTERN struct rbnode *rbtree_find(const struct rbtree *tree,
+		const void *key);
+
+/*!
+ * Returns a pointer to the first (leftmost) node in a red-black tree. This is
+ * an O(log(n)) operation.
+ *
+ * \see rbtree_last()
+ */
+LELY_UTIL_EXTERN struct rbnode *rbtree_first(const struct rbtree *tree);
+
+/*!
+ * Returns a pointer to the last (rightmost) node in a red-black tree. This is
+ * an O(log(n)) operation.
+ *
+ * \see rbtree_first()
+ */
+LELY_UTIL_EXTERN struct rbnode *rbtree_last(const struct rbtree *tree);
+
+/*!
+ * Returns a pointer to the root node in a red-black tree. This is an O(1)
+ * operation.
+ */
+static inline struct rbnode *rbtree_root(const struct rbtree *tree);
+
+/*!
+ * Iterates over each node in a red-black tree in ascending order.
+ *
+ * \param tree a pointer to a red-black tree.
+ * \param node the name of the pointer to the nodes. This variable is declared
+ *             in the scope of the loop.
+ *
+ * \see rbtree_first(), rbnode_next()
+ */
+#define rbtree_foreach(tree, node) \
+	_rbtree_foreach(tree, node, __LINE__)
+#define _rbtree_foreach(tree, node, line) \
+	__rbtree_foreach(tree, node, line)
+#define __rbtree_foreach(tree, node, line) \
+	for (struct rbnode *(node) = rbtree_first(tree), \
+			*__rbnode_next_##line = (node) \
+			? rbnode_next(node) : NULL; \
+			(node); (node) = __rbnode_next_##line, \
+			__rbnode_next_##line = (node) \
+			? rbnode_next(node) : NULL)
+
+static inline void
+rbnode_init(struct rbnode *node, const void *key)
+{
+	node->key = key;
+}
+
+static inline void
+rbtree_init(struct rbtree *tree, cmp_t cmp)
+{
+	tree->cmp = cmp;
+	tree->root = NULL;
+	tree->num_nodes = 0;
+}
+
+static inline int
+rbtree_empty(const struct rbtree *tree)
+{
+	return !rbtree_size(tree);
+}
+
+static inline size_t
+rbtree_size(const struct rbtree *tree)
+{
+	return tree->num_nodes;
+}
+
+static inline struct rbnode *
+rbtree_root(const struct rbtree *tree)
+{
+	return tree->root;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ src/rbtree.c
@@ -0,0 +1,445 @@
+/*!\file
+ * This file is part of the utilities library; it contains the implementation of
+ * the red-black tree.
+ *
+ * \see lely/util/rbtree.h
+ *
+ * \copyright 2016 Lely Industries N.V.
+ *
+ * \author J. S. Seldenthuis <jseldenthuis@lely.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "util.h"
+#include <lely/util/rbtree.h>
+
+#include <assert.h>
+
+//! Returns the parent of \a node, or NULL if \a node is NULL.
+static inline struct rbnode *rbnode_get_parent(const struct rbnode *node);
+
+/*!
+ * Sets the parent of \a node to \a parent. If \a node is NULL, this function
+ * has no effect.
+ */
+static void rbnode_set_parent(struct rbnode *node, const struct rbnode *parent);
+
+//! Returns the color of \a node, or 0 (black) if \a node is NULL.
+static inline int rbnode_get_color(const struct rbnode *node);
+
+/*!
+ * Sets the color of \a node to 0 (black) if \a color 0, or to 1 (red)
+ * otherwise. If \a node is NULL, this function has no effect.
+ */
+static inline void rbnode_set_color(struct rbnode *node, int color);
+
+//! Returns the leftmost descendant of \a node.
+static struct rbnode *rbnode_min(struct rbnode *node);
+
+//! Returns the rightmost descendant of \a node.
+static struct rbnode *rbnode_max(struct rbnode *node);
+
+/*!
+ * Rotates \a node counter-clockwise (left), i.e., it is replaced in the tree by
+ * its right child, while becoming that child's left child.
+ */
+static void rbtree_rol(struct rbtree *tree, struct rbnode *node);
+
+/*!
+ * Rotates \a node clockwise (right), i.e., it is replaced in the tree by its
+ * left child, while becoming that child's right child.
+ */
+static void rbtree_ror(struct rbtree *tree, struct rbnode *node);
+
+//! Replaces \a old_node by \a new_node in \a tree by updating its parent.
+static void rbtree_replace(struct rbtree *tree, struct rbnode *old_node,
+		struct rbnode *new_node);
+
+LELY_UTIL_EXPORT struct rbnode *
+rbnode_prev(const struct rbnode *node)
+{
+	assert(node);
+
+	// If the node has a left subtree, find its rightmost descendant.
+	if (node->left)
+		return rbnode_max(node->left);
+	// Find the lowest ancestor that has the node in its right subtree.
+	struct rbnode *parent = rbnode_get_parent(node);
+	while (parent && node == parent->left) {
+		node = parent;
+		parent = rbnode_get_parent(node);
+	}
+	return parent;
+}
+
+LELY_UTIL_EXPORT struct rbnode *
+rbnode_next(const struct rbnode *node)
+{
+	assert(node);
+
+	// If the node has a right subtree, find its leftmost descendant.
+	if (node->right)
+		return rbnode_min(node->right);
+	// Find the lowest ancestor that has the node in its left subtree.
+	struct rbnode *parent = rbnode_get_parent(node);
+	while (parent && node == parent->right) {
+		node = parent;
+		parent = rbnode_get_parent(node);
+	}
+	return parent;
+}
+
+LELY_UTIL_EXPORT void
+rbtree_insert(struct rbtree *tree, struct rbnode *node)
+{
+	assert(tree);
+	assert(tree->cmp);
+	assert(node);
+
+	// Find the parent of the new node.
+	struct rbnode *parent = NULL;
+	struct rbnode *next = tree->root;
+	while (next) {
+		parent = next;
+		next = tree->cmp(node->key, next->key) < 0
+				? next->left : next->right;
+	}
+	// Attach the node to its parent.
+	if (!parent)
+		tree->root = node;
+	else if (tree->cmp(node->key, parent->key) < 0)
+		parent->left = node;
+	else
+		parent->right = node;
+	// Initialize the node.
+	rbnode_set_parent(node, parent);
+	rbnode_set_color(node, 1);
+	node->left = NULL;
+	node->right = NULL;
+	// Fix violations of the red-black properties.
+	while (rbnode_get_color(parent)) {
+		struct rbnode *gparent = rbnode_get_parent(parent);
+		if (parent == gparent->left) {
+			if (rbnode_get_color(gparent->right)) {
+				// Case 1: flip colors.
+				rbnode_set_color(gparent, 1);
+				rbnode_set_color(parent, 0);
+				rbnode_set_color(gparent->right, 0);
+				node = gparent;
+			} else {
+				if (node == parent->right) {
+					// Case 2: left rotate at grandparent.
+					node = parent;
+					rbtree_rol(tree, node);
+					parent = rbnode_get_parent(node);
+					gparent = rbnode_get_parent(parent);
+				}
+				// Case 3: right rotate at grandparent.
+				rbnode_set_color(gparent, 1);
+				rbnode_set_color(parent, 0);
+				rbtree_ror(tree, gparent);
+			}
+		} else {
+			if (rbnode_get_color(gparent->left)) {
+				// Case 1: flip colors.
+				rbnode_set_color(gparent, 1);
+				rbnode_set_color(parent, 0);
+				rbnode_set_color(gparent->left, 0);
+				node = gparent;
+			} else {
+				if (node == parent->left) {
+					// Case 2: right rotate at grandparent.
+					node = parent;
+					rbtree_ror(tree, node);
+					parent = rbnode_get_parent(node);
+					gparent = rbnode_get_parent(parent);
+				}
+				// Case 3: left rotate at grandparent.
+				rbnode_set_color(gparent, 1);
+				rbnode_set_color(parent, 0);
+				rbtree_rol(tree, gparent);
+			}
+		}
+		parent = rbnode_get_parent(node);
+	}
+	rbnode_set_color(tree->root, 0);
+
+	tree->num_nodes++;
+}
+
+LELY_UTIL_EXPORT void
+rbtree_remove(struct rbtree *tree, struct rbnode *node)
+{
+	assert(tree);
+	assert(node);
+
+	int color = rbnode_get_color(node);
+	struct rbnode *parent = rbnode_get_parent(node);
+	// Remove the node from the tree. After removal, node points to the
+	// subtree in which we might have introduced red-black property
+	// violations.
+	if (!node->left) {
+		// There is no left subtree, so we can replace the node by its
+		// right subtree.
+		rbtree_replace(tree, node, node->right);
+		node = node->right;
+	} else if (!node->right) {
+		// There is no right subtree, so we can replace the node by its
+		// left subtree.
+		rbtree_replace(tree, node, node->left);
+		node = node->left;
+	} else {
+		// There is both a left and a right subtree, so we replace the
+		// node by its successor. First, find the successor and give it
+		// the same color as the node to be removed.
+		struct rbnode *next = rbnode_min(node->right);
+		color = rbnode_get_color(next);
+		rbnode_set_color(next, rbnode_get_color(node));
+		struct rbnode *tmp = next->right;
+		if (rbnode_get_parent(next) == node) {
+			parent = next;
+		} else {
+			parent = rbnode_get_parent(next);
+			// If the successor is not a child of the node to be
+			// removed, we remove it from its parent.
+			rbtree_replace(tree, next, next->right);
+			next->right = node->right;
+			rbnode_set_parent(next->right, next);
+		}
+		// Replace the node to be removed by its successor.
+		rbtree_replace(tree, node, next);
+		// The successor has no left subtree (by definition). Attach the
+		// left subtree of the node to be removed to the successor.
+		next->left = node->left;
+		rbnode_set_parent(next->left, next);
+		node = tmp;
+	}
+	// Fix violations of the red-black properties. This can only occur if
+	// the removed node (or its successor) was black.
+	if (color)
+		return;
+	while (node != tree->root && !rbnode_get_color(node)) {
+		if (node == parent->left) {
+			struct rbnode *tmp = parent->right;
+			if (rbnode_get_color(tmp)) {
+				// Case 1: left rotate at parent.
+				rbnode_set_color(parent, 1);
+				rbnode_set_color(tmp, 0);
+				rbtree_rol(tree, parent);
+				tmp = parent->right;
+			}
+			if (!rbnode_get_color(tmp->left)
+					&& !rbnode_get_color(tmp->right)) {
+				// Case 2: color flip at sibling.
+				rbnode_set_color(tmp, 1);
+				node = parent;
+				parent = rbnode_get_parent(node);
+			} else {
+				if (!rbnode_get_color(tmp->right)) {
+					// Case 3: right rotate at sibling.
+					rbnode_set_color(tmp, 1);
+					rbnode_set_color(tmp->left, 0);
+					rbtree_ror(tree, tmp);
+					tmp = parent->right;
+				}
+				// Case 4: left rotate at parent and color flip.
+				rbnode_set_color(tmp,
+						rbnode_get_color(parent));
+				rbnode_set_color(parent, 0);
+				rbnode_set_color(tmp->right, 0);
+				rbtree_rol(tree, parent);
+				node = tree->root;
+			}
+		} else {
+			struct rbnode *tmp = parent->left;
+			if (rbnode_get_color(tmp)) {
+				// Case 1: right rotate at parent.
+				rbnode_set_color(parent, 1);
+				rbnode_set_color(tmp, 0);
+				rbtree_ror(tree, parent);
+				tmp = parent->left;
+			}
+			if (!rbnode_get_color(tmp->right)
+					&& !rbnode_get_color(tmp->left)) {
+				// Case 2: color flip at sibling.
+				rbnode_set_color(tmp, 1);
+				node = parent;
+				parent = rbnode_get_parent(node);
+			} else {
+				if (!rbnode_get_color(tmp->left)) {
+					// Case 3: left rotate at sibling.
+					rbnode_set_color(tmp, 1);
+					rbnode_set_color(tmp->right, 0);
+					rbtree_rol(tree, tmp);
+					tmp = parent->left;
+				}
+				// Case 4: right rotate at parent and color
+				// flip.
+				rbnode_set_color(tmp,
+						rbnode_get_color(parent));
+				rbnode_set_color(parent, 0);
+				rbnode_set_color(tmp->left, 0);
+				rbtree_ror(tree, parent);
+				node = tree->root;
+			}
+		}
+	}
+	rbnode_set_color(node, 0);
+
+	tree->num_nodes--;
+}
+
+LELY_UTIL_EXPORT struct rbnode *
+rbtree_find(const struct rbtree *tree, const void *key)
+{
+	assert(tree);
+	assert(tree->cmp);
+
+	struct rbnode *node = tree->root;
+	while (node) {
+		int cmp = tree->cmp(key, node->key);
+		if (!cmp)
+			break;
+		node = cmp < 0 ? node->left : node->right;
+	}
+	return node;
+}
+
+LELY_UTIL_EXPORT struct rbnode *
+rbtree_first(const struct rbtree *tree)
+{
+	assert(tree);
+
+	return tree->root ? rbnode_min(tree->root) : NULL;
+}
+
+LELY_UTIL_EXPORT struct rbnode *
+rbtree_last(const struct rbtree *tree)
+{
+	assert(tree);
+
+	return tree->root ? rbnode_max(tree->root) : NULL;
+}
+
+static inline struct rbnode *
+rbnode_get_parent(const struct rbnode *node)
+{
+	return node ? (struct rbnode *)(node->parent & ~(uintptr_t)1) : NULL;
+}
+
+static void
+rbnode_set_parent(struct rbnode *node, const struct rbnode *parent)
+{
+	if (__unlikely(!node))
+		return;
+
+	node->parent = (uintptr_t)parent | rbnode_get_color(node);
+}
+
+static inline int
+rbnode_get_color(const struct rbnode *node)
+{
+	return node ? node->parent & (uintptr_t)1 : 0;
+}
+
+static inline void
+rbnode_set_color(struct rbnode *node, int color)
+{
+	if (__unlikely(!node))
+		return;
+
+	if (color)
+		node->parent |= (uintptr_t)1;
+	else
+		node->parent &= ~(uintptr_t)1;
+}
+
+static struct rbnode *
+rbnode_min(struct rbnode *node)
+{
+	assert(node);
+
+	while (node->left)
+		node = node->left;
+	return node;
+}
+
+static struct rbnode *
+rbnode_max(struct rbnode *node)
+{
+	assert(node);
+
+	while (node->right)
+		node = node->right;
+	return node;
+}
+
+static void
+rbtree_rol(struct rbtree *tree, struct rbnode *node)
+{
+	assert(tree);
+	assert(node);
+	assert(node->right);
+
+	struct rbnode *tmp = node->right;
+	node->right = tmp->left;
+	rbnode_set_parent(tmp->left, node);
+	tmp->left = node;
+	struct rbnode *parent = rbnode_get_parent(node);
+	rbnode_set_parent(node, tmp);
+	rbnode_set_parent(tmp, parent);
+	if (!parent)
+		tree->root = tmp;
+	else if (node == parent->left)
+		parent->left = tmp;
+	else
+		parent->right = tmp;
+}
+
+static void
+rbtree_ror(struct rbtree *tree, struct rbnode *node)
+{
+	assert(tree);
+	assert(node);
+	assert(node->left);
+
+	struct rbnode *tmp = node->left;
+	node->left = tmp->right;
+	rbnode_set_parent(tmp->right, node);
+	tmp->right = node;
+	struct rbnode *parent = rbnode_get_parent(node);
+	rbnode_set_parent(node, tmp);
+	rbnode_set_parent(tmp, parent);
+	if (!parent)
+		tree->root = tmp;
+	else if (node == parent->right)
+		parent->right = tmp;
+	else
+		parent->left = tmp;
+}
+
+static void
+rbtree_replace(struct rbtree *tree, struct rbnode *old_node,
+		struct rbnode *new_node)
+{
+	struct rbnode *parent = rbnode_get_parent(old_node);
+	if (!parent)
+		tree->root = new_node;
+	else if (old_node == parent->left)
+		parent->left = new_node;
+	else
+		parent->right = new_node;
+	rbnode_set_parent(new_node, parent);
+}
+
