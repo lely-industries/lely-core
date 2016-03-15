2e735ece7010cf66bb90a61c048ff42d1a4dc453
J. S. Seldenthuis <jseldenthuis@lely.com>
2016-03-15 13:25:17 +0100

    add <lely/libc/time.h> and its implementation

---
+++ include/lely/libc/time.h
@@ -0,0 +1,72 @@
+/*!\file
+ * This header file is part of the C11 and POSIX compatibility library; it
+ * includes `<time.h>` and defines any missing functionality.
+ *
+ * \copyright 2016 Lely Industries N.V.
+ *
+ * \author J. S. Seldenthuis <jseldenthuis@lely.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef LELY_LIBC_TIME_H
+#define LELY_LIBC_TIME_H
+
+#include <lely/libc/libc.h>
+
+#include <time.h>
+
+#if !(__STDC_VERSION__ >= 201112L) \
+		&& !(_POSIX_C_SOURCE >= 199309L || defined(__CYGWIN__) \
+		|| defined(_TIMESPEC_DEFINED) || defined(__timespec_defined))
+//! A time type with nanosecond resolution.
+struct timespec {
+	//! Whole seconds (>= 0).
+	time_t tv_sec;
+	//! Nanoseconds [0, 999999999].
+	long tv_nsec;
+};
+#endif
+
+#if !(__STDC_VERSION__ >= 201112L) && !defined(__USE_ISOC11)
+
+#ifndef TIME_UTC
+//! An integer constant greater than 0 that designates the UTC time base.
+#define TIME_UTC	1
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*!
+ * Sets the interval at \a ts to hold the current calendar time based on the
+ * specified time base.
+ *
+ * If base is `TIME_UTC`, the \a tv_sec member is set to the number of seconds
+ * since an implementation defined epoch, truncated to a whole value and the
+ * \a tv_nsec member is set to the integral number of nanoseconds, rounded to
+ * the resolution of the system clock.
+ *
+ * \returns the nonzero value \a base on success; otherwise, it returns zero.
+ */
+LELY_LIBC_EXTERN int __cdecl timespec_get(struct timespec *ts, int base);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // !(__STDC_VERSION__ >= 201112L) && !__USE_ISOC11
+
+#endif
+
---
+++ src/time.c
@@ -0,0 +1,67 @@
+/*!\file
+ * This file is part of the C11 and POSIX compatibility library.
+ *
+ * \see lely/libc/time.h
+ *
+ * \copyright 2016 Lely Industries N.V.
+ *
+ * \author J. S. Seldenthuis <jseldenthuis@lely.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "libc.h"
+#include <lely/libc/time.h>
+
+#if !(__STDC_VERSION__ >= 201112L) && !defined(__USE_ISOC11)
+
+#ifdef _WIN32
+
+#include <lely/libc/stdint.h>
+
+LELY_LIBC_EXPORT int __cdecl
+timespec_get(struct timespec *ts, int base)
+{
+	if (__unlikely(base != TIME_UTC))
+		return 0;
+
+	FILETIME ft;
+	GetSystemTimeAsFileTime(&ft);
+	uint64_t nsec = 100 * ((uint64_t)ft.dwHighDateTime << 32
+			| (uint64_t)ft.dwLowDateTime);
+	ts->tv_sec = nsec / 1000000000l;
+	ts->tv_nsec = nsec % 1000000000l;
+	// Convert file time (seconds since 00:00:00 UTC on January 1, 1601), to
+	// the Unix epoch (seconds since 00:00:00 UTC on January 1, 1970). This
+	// is a difference of 369 years and 89 leap days.
+	ts->tv_sec -= (int64_t)(369 * 365 + 89) * 24 * 60 * 60;
+
+	return base;
+}
+
+#elif defined(_POSIX_TIMERS)
+
+LELY_LIBC_EXPORT int __cdecl
+timespec_get(struct timespec *ts, int base)
+{
+	if (__unlikely(base != TIME_UTC))
+		return 0;
+	if (__unlikely(clock_gettime(CLOCK_REALTIME, ts) == -1))
+		return 0;
+	return base;
+}
+
+#endif
+
+#endif // !(__STDC_VERSION__ >= 201112L) && !defined(__USE_ISOC11)
+
