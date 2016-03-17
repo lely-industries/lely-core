2d6cea969ca6b358e0e593eba3026badb07aee43
J. S. Seldenthuis <jseldenthuis@lely.com>
2016-03-17 11:29:58 +0100

    add configuration functions

---
+++ src/config.c
@@ -0,0 +1,314 @@
+/*!\file
+ * This file is part of the utilities library; it contains the implementation of
+ * the configuration functions.
+ *
+ * \see lely/util/config.h
+ *
+ * \copyright 2016 Lely Industries N.V.
+ *
+ * \author J. S. Seldenthuis <jseldenthuis@lely.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "util.h"
+#include <lely/util/config.h>
+#include <lely/util/errnum.h>
+#include <lely/util/rbtree.h>
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+//! A configuration struct.
+struct __config {
+	//! The tree containing the sections.
+	struct rbtree tree;
+};
+
+//! A section in a configuration struct.
+struct config_section {
+	//! The node of this section in the tree of sections.
+	struct rbnode node;
+	//! The tree containing the entries.
+	struct rbtree tree;
+};
+
+static struct rbnode *config_section_create(config_t *config,
+		const char *name);
+static void config_section_destroy(struct rbnode *node);
+
+static const char *config_section_set(struct rbnode *node, const char *key,
+		const char *value);
+
+//! An entry in a configuration section.
+struct config_entry {
+	//! The node of this entry in the tree of entries.
+	struct rbnode node;
+	//! The value of the entry.
+	char *value;
+};
+
+static struct rbnode *config_entry_create(struct config_section *section,
+		const char *key, const char *value);
+static void config_entry_destroy(struct rbnode *node);
+
+LELY_UTIL_EXPORT void *
+__config_alloc(void)
+{
+	return malloc(sizeof(struct __config));
+}
+
+LELY_UTIL_EXPORT void
+__config_free(void *ptr)
+{
+	free(ptr);
+}
+
+LELY_UTIL_EXPORT struct __config *
+__config_init(struct __config *config, int flags)
+{
+	assert(config);
+
+	rbtree_init(&config->tree,
+			(flags & CONFIG_CASE) ? str_case_cmp : str_cmp);
+
+	if (__unlikely(!config_section_create(config, "")))
+		return NULL;
+
+	return config;
+}
+
+LELY_UTIL_EXPORT void
+__config_fini(struct __config *config)
+{
+	assert(config);
+
+	rbtree_foreach(&config->tree, node) {
+		rbtree_remove(&config->tree, node);
+		config_section_destroy(node);
+	}
+}
+
+LELY_UTIL_EXPORT config_t *
+config_create(int flags)
+{
+	errc_t errc = 0;
+
+	config_t *config = __config_alloc();
+	if (__unlikely(!config)) {
+		errc = get_errc();
+		goto error_alloc_config;
+	}
+
+	if (__unlikely(!__config_init(config, flags))) {
+		errc = get_errc();
+		goto error_init_config;
+	}
+
+	return config;
+
+error_init_config:
+	__config_free(config);
+error_alloc_config:
+	set_errc(errc);
+	return NULL;
+}
+
+LELY_UTIL_EXPORT void
+config_destroy(config_t *config)
+{
+	if (config) {
+		__config_fini(config);
+		__config_free(config);
+	}
+}
+
+LELY_UTIL_EXPORT const char *
+config_get(const config_t *config, const char *section, const char *key)
+{
+	assert(config);
+
+	if (!section)
+		section = "";
+
+	if (__unlikely(!key))
+		return NULL;
+
+	const struct rbtree *tree = &config->tree;
+	struct rbnode *node;
+
+	node = rbtree_find(tree, section);
+	if (__unlikely(!node))
+		return NULL;
+	tree = &structof(node, struct config_section, node)->tree;
+
+	node = rbtree_find(tree, key);
+	if (__unlikely(!node))
+		return NULL;
+	return structof(node, struct config_entry, node)->value;
+}
+
+LELY_UTIL_EXPORT const char *
+config_set(config_t *config, const char *section, const char *key,
+		const char *value)
+{
+	assert(config);
+
+	if (!section)
+		section = "";
+
+	if (__unlikely(!key))
+		return NULL;
+
+	struct rbnode *node;
+
+	node = rbtree_find(&config->tree, section);
+	// Only create a section if we are not removing an entry.
+	if (!node && value)
+		node = config_section_create(config, section);
+
+	return node ? config_section_set(node, key, value) : NULL;
+}
+
+static struct rbnode *
+config_section_create(config_t *config, const char *name)
+{
+	assert(config);
+	assert(name);
+
+	errc_t errc = 0;
+
+	struct config_section *section = malloc(sizeof(*section));
+	if (__unlikely(!section)) {
+		errc = get_errc();
+		goto error_alloc_section;
+	}
+
+	struct rbnode *node = &section->node;
+
+	node->key = malloc(strlen(name) + 1);
+	if (__unlikely(!node->key)) {
+		errc = get_errc();
+		goto error_alloc_key;
+	}
+	strcpy((char *)node->key, name);
+
+	rbtree_init(&section->tree, config->tree.cmp);
+
+	rbtree_insert(&config->tree, node);
+
+	return node;
+
+error_alloc_key:
+	free(section);
+error_alloc_section:
+	set_errc(errc);
+	return NULL;
+}
+
+static void
+config_section_destroy(struct rbnode *node)
+{
+	assert(node);
+	struct config_section *section =
+			structof(node, struct config_section, node);
+
+	free((char *)node->key);
+
+	rbtree_foreach(&section->tree, node) {
+		rbtree_remove(&section->tree, node);
+		config_entry_destroy(node);
+	}
+
+	free(section);
+}
+
+static const char *
+config_section_set(struct rbnode *node, const char *key, const char *value)
+{
+	assert(node);
+	struct config_section *section =
+			structof(node, struct config_section, node);
+
+	// Remove the existing entry.
+	node = rbtree_find(&section->tree, key);
+	if (node) {
+		rbtree_remove(&section->tree, node);
+		config_entry_destroy(node);
+	}
+	if (!value)
+		return NULL;
+
+	node = config_entry_create(section, key, value);
+	return __likely(node)
+			? structof(node, struct config_entry, node)->value
+			: NULL;
+}
+
+static struct rbnode *
+config_entry_create(struct config_section *section, const char *key,
+		const char *value)
+{
+	assert(section);
+	assert(key);
+
+	errc_t errc = 0;
+
+	struct config_entry *entry = malloc(sizeof(*entry));
+	if (__unlikely(!entry)) {
+		errc = get_errc();
+		goto error_alloc_entry;
+	}
+
+	struct rbnode *node = &entry->node;
+
+	node->key = malloc(strlen(key) + 1);
+	if (__unlikely(!node->key)) {
+		errc = get_errc();
+		goto error_alloc_key;
+	}
+	strcpy((char *)node->key, key);
+
+	entry->value = malloc(strlen(value) + 1);
+	if (__unlikely(!entry->value)) {
+		errc = get_errc();
+		goto error_alloc_value;
+	}
+	strcpy(entry->value, value);
+
+	rbtree_insert(&section->tree, node);
+
+	return node;
+
+error_alloc_value:
+	free((char *)node->key);
+error_alloc_key:
+	free(entry);
+error_alloc_entry:
+	set_errc(errc);
+	return NULL;
+}
+
+static void
+config_entry_destroy(struct rbnode *node)
+{
+	assert(node);
+	struct config_entry *entry = structof(node, struct config_entry, node);
+
+	free((char *)node->key);
+
+	free(entry->value);
+	free(entry);
+}
+
