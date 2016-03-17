9ea8e57e0bcf7e352626b6663f60446c2160101b
J. S. Seldenthuis <jseldenthuis@lely.com>
2016-03-17 12:08:03 +0100

    add memory buffer implementation

---
+++ include/lely/util/membuf.h
@@ -0,0 +1,196 @@
+/*!\file
+ * This header file is part of the utilities library; it contains the memory
+ * buffer declarations.
+ *
+ * \copyright 2016 Lely Industries N.V.
+ *
+ * \author J. S. Seldenthuis <jseldenthuis@lely.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef LELY_UTIL_MEMBUF_H
+#define LELY_UTIL_MEMBUF_H
+
+#include <lely/util/util.h>
+
+#include <stddef.h>
+#include <string.h>
+
+//! A memory buffer.
+struct membuf {
+	//! A pointer to one past the last byte written to the buffer.
+	char *cur;
+	//! A pointer to the first byte in the buffer.
+	char *begin;
+	//! A pointer to one past the last byte in the buffer.
+	char *end;
+};
+
+//! The static initializer for struct #membuf.
+#define MEMBUF_INIT	{ NULL, NULL, NULL }
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//! Initializes a memory buffer. \see membuf_fini()
+static inline void membuf_init(struct membuf *buf);
+
+//! Finalizes a memory buffer. \see membuf_init()
+LELY_UTIL_EXTERN void membuf_fini(struct membuf *buf);
+
+//! Returns a pointer to the first byte in a memory buffer.
+static inline void *membuf_begin(const struct membuf *buf);
+
+//! Clears a memory buffer. \see membuf_flush()
+static inline void membuf_clear(struct membuf *buf);
+
+//! Returns the total number of bytes written to a memory buffer.
+static inline size_t membuf_size(const struct membuf *buf);
+
+//! Returns the number of unused bytes remaining in a memory buffer.
+static inline size_t membuf_capacity(const struct membuf *buf);
+
+/*!
+ * Resizes a memory buffer, if necessary, to make room for at least an
+ * additional \a size bytes. This function may also shrink the buffer if it is
+ * larger than required. Although this functions preserves the data already
+ * written to the buffer, it invalidates the pointers obtained from previous
+ * calls to membuf_alloc().
+ *
+ * \param buf  a pointer to a memory buffer.
+ * \param size the required capacity of the buffer.
+ *
+ * \returns the new capacity of the buffer, or 0 on error. The new capacity can
+ * be larger than the requested capacity.
+ */
+LELY_UTIL_EXTERN size_t membuf_reserve(struct membuf *buf, size_t size);
+
+/*!
+ * Adjusts the position indicator of a memory buffer by \a offset bytes. The
+ * offset will be truncated to a valid range: if negative, -\a offset must not
+ * exceed membuf_size(); if positive, \a offset must not exceed
+ * membuf_capacity().
+ *
+ * \param buf    a pointer to a memory buffer.
+ * \param offset the offset (in bytes) with respect to the current position
+ *               indicator.
+ *
+ * \returns the actual applied offset.
+ */
+static inline ptrdiff_t membuf_seek(struct membuf *buf, ptrdiff_t offset);
+
+/*!
+ * Creates region of *\a size bytes in a memory buffer, starting at the current
+ * position indicator given by membuf_size(), and sets the indicator to the end
+ * of the requested region. If the requested size is larger than the current
+ * capacity, *\a size will be updated with the actual size of the region. If the
+ * requested size turns out to be too large or too small, the region can be
+ * adjusted with membuf_seek().
+ *
+ * \param buf  a pointer to a memory buffer.
+ * \param size the address of the value containing the requested size. On exit,
+ *             *\a size is updated with the actual size.
+ *
+ * \returns a pointer to a memory region of at least *\a size consecutive
+ * bytes (which may be 0). Note that this pointer is only valid until the next
+ * call to membuf_reserve().
+ */
+static inline void *membuf_alloc(struct membuf *buf, size_t *size);
+
+/*!
+ * Writes data to a memory buffer. Writing starts at the current position
+ * indicator given by membuf_size().
+ *
+ * \param buf  a pointer to a memory buffer.
+ * \param ptr  a pointer to the data to be copied.
+ * \param size the number of bytes to be written.
+ *
+ * \returns the number of bytes written, which may be smaller than \a size in
+ * case of insufficient capacity.
+ */
+static inline size_t membuf_write(struct membuf *buf, const void *ptr,
+		size_t size);
+
+//! Flushes \a size bytes from the beginning of a memory buffer.
+LELY_UTIL_EXTERN void membuf_flush(struct membuf *buf, size_t size);
+
+static inline void
+membuf_init(struct membuf *buf)
+{
+	buf->begin = buf->end = buf->cur = NULL;
+}
+
+static inline void *
+membuf_begin(const struct membuf *buf)
+{
+	return buf->begin;
+}
+
+static inline void
+membuf_clear(struct membuf *buf)
+{
+	buf->cur = buf->begin;
+}
+
+static inline size_t
+membuf_size(const struct membuf *buf)
+{
+	return buf->cur - buf->begin;
+}
+
+static inline size_t
+membuf_capacity(const struct membuf *buf)
+{
+	return buf->end - buf->cur;
+}
+
+static inline ptrdiff_t
+membuf_seek(struct membuf *buf, ptrdiff_t offset)
+{
+	char *cur = buf->cur + offset;
+	if (__unlikely(cur - buf->begin < 0)) {
+		cur = buf->begin;
+		offset = cur - buf->cur;
+	} else if (__unlikely(buf->end - cur < 0)) {
+		cur = buf->end;
+		offset = cur - buf->cur;
+	}
+	buf->cur = cur;
+
+	return offset;
+}
+
+static inline void *
+membuf_alloc(struct membuf *buf, size_t *size)
+{
+	void *cur = buf->cur;
+	*size = membuf_seek(buf, *size);
+	return cur;
+}
+
+static inline size_t
+membuf_write(struct membuf *buf, const void *ptr, size_t size)
+{
+	void *cur = membuf_alloc(buf, &size);
+	memcpy(cur, ptr, size);
+	return size;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ src/membuf.c
@@ -0,0 +1,80 @@
+/*!\file
+ * This file is part of the utilities library; it contains the implementation of
+ * the memory buffer.
+ *
+ * \see lely/util/membuf.h
+ *
+ * \copyright 2016 Lely Industries N.V.
+ *
+ * \author J. S. Seldenthuis <jseldenthuis@lely.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "util.h"
+#include <lely/util/membuf.h>
+
+#include <assert.h>
+#include <stdlib.h>
+
+#ifndef LELY_MEMBUF_SIZE
+//! The initial size (in bytes) of a memory buffer.
+#define LELY_MEMBUF_SIZE	16
+#endif
+
+LELY_UTIL_EXPORT void
+membuf_fini(struct membuf *buf)
+{
+	assert(buf);
+
+	free(buf->begin);
+}
+
+LELY_UTIL_EXPORT size_t
+membuf_reserve(struct membuf *buf, size_t size)
+{
+	assert(buf);
+
+	size_t capacity = membuf_capacity(buf);
+	if (__likely(size <= capacity))
+		return capacity;
+
+	// The required size equals the size of the data already in the buffer,
+	// plus the data to be added. To limit the number of allocations, we
+	// keep doubling the size of the buffer until it is large enough.
+	size_t buf_size = LELY_MEMBUF_SIZE;
+	while (buf_size < membuf_size(buf) + size)
+		buf_size *= 2;
+
+	char *begin = realloc(buf->begin, buf_size);
+	if (__unlikely(!begin))
+		return 0;
+
+	buf->cur = begin + membuf_size(buf);
+	buf->begin = begin;
+	buf->end = buf->begin + buf_size;
+
+	return membuf_capacity(buf);
+}
+
+LELY_UTIL_EXPORT void
+membuf_flush(struct membuf *buf, size_t size)
+{
+	assert(buf);
+
+	size = MIN(size, membuf_size(buf));
+	if (size)
+		memmove(buf->begin, buf->begin + size, membuf_size(buf) - size);
+	buf->cur -= size;
+}
+
