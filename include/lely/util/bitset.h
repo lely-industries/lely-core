29f86442d2264512444886e830144769b1cfc57b
J. S. Seldenthuis <jseldenthuis@lely.com>
2016-03-18 17:44:21 +0100

    add bitset

---
+++ include/lely/util/bitset.h
@@ -0,0 +1,117 @@
+/*!\file
+ * This header file is part of the utilities library; it contains the bitset
+ * declarations.
+ *
+ * \copyright 2016 Lely Industries N.V.
+ *
+ * \author J. S. Seldenthuis <jseldenthuis@lely.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef LELY_UTIL_BITSET_H
+#define LELY_UTIL_BITSET_H
+
+#include <lely/util/util.h>
+
+//! A variable-sized bitset.
+struct bitset {
+	//! The number of integers in #bits.
+	int size;
+	//! An array of #size integers.
+	unsigned int *bits;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*!
+ * Initializes a bitset. On success, all bits are cleared.
+ *
+ * \param set  a pointer to a bitset.
+ * \param size the requested size (in number of bits) of the set. This number is
+ *             rounded up to the nearest multiple of `sizeof(int) * CHAR_BIT`.
+ *
+ * \returns 0 on success, or -1 on error. In the latter case, the error number
+ * can be obtained with get_errnum().
+ *
+ * \see bitset_fini()
+ */
+LELY_UTIL_EXTERN int bitset_init(struct bitset *set, int size);
+
+//! Finalizes a bitset. \see bitset_init().
+LELY_UTIL_EXTERN void bitset_fini(struct bitset *set);
+
+//! Returns the size (in number of bits) of \a set.
+LELY_UTIL_EXTERN int bitset_size(const struct bitset *set);
+
+/*!
+ * Resizes a bitset. On success, if the set has grown, all new bits are cleared.
+ *
+ * \param set  a pointer to a bitset.
+ * \param size the requester size (in number of bits) of the set. This number is
+ *             rounded up to the nearest multiple of `sizeof(int) * CHAR_BIT`.
+ *
+ * \returns the new size (in number of bits) of the bitset, or 0 on error.
+ */
+LELY_UTIL_EXTERN int bitset_resize(struct bitset *set, int size);
+
+//! Returns 1 if bit \a n in \a set is set, and 0 otherwise.
+LELY_UTIL_EXTERN int bitset_test(const struct bitset *set, int n);
+
+//! Sets bit \a n in \a set.
+LELY_UTIL_EXTERN void bitset_set(struct bitset *set, int n);
+
+//! Sets all bits in \a set.
+LELY_UTIL_EXTERN void bitset_set_all(struct bitset *set);
+
+//! Clears bit \a n in \a set.
+LELY_UTIL_EXTERN void bitset_clr(struct bitset *set, int n);
+
+//! Clears all bits in \a set.
+LELY_UTIL_EXTERN void bitset_clr_all(struct bitset *set);
+
+//! Flip all bits in \a set.
+LELY_UTIL_EXTERN void bitset_compl(struct bitset *set);
+
+/*!
+ * Returns the index (starting from one) of the first set bit in \a set, or 0 if
+ * all bits are zero.
+ */
+LELY_UTIL_EXTERN int bitset_ffs(const struct bitset *set);
+
+/*!
+ * Returns the index (starting from one) of the first zero bit in \a set, or 0
+ * if all bits are set.
+ */
+LELY_UTIL_EXTERN int bitset_ffz(const struct bitset *set);
+
+/*!
+ * Returns the index (starting from one) of the first set bit higher or equal to
+ * \a n in \a set, or 0 if all bits starting from \a n are zero.
+ */
+LELY_UTIL_EXTERN int bitset_fns(const struct bitset *set, int n);
+
+/*!
+ * Returns the index (starting from one) of the first zero bit higher or equal
+ * to \a n in \a set, or 0 if all bits starting from \a n are set.
+ */
+LELY_UTIL_EXTERN int bitset_fnz(const struct bitset *set, int n);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ src/bitset.c
@@ -0,0 +1,220 @@
+/*!\file
+ * This file is part of the utilities library; it contains the implementation of
+ * the bitset functions.
+ *
+ * \see lely/util/bitset.h
+ *
+ * \copyright 2016 Lely Industries N.V.
+ *
+ * \author J. S. Seldenthuis <jseldenthuis@lely.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "util.h"
+#include <lely/libc/strings.h>
+#include <lely/util/bitset.h>
+
+#include <assert.h>
+#include <stdlib.h>
+
+#undef INT_BIT
+#define INT_BIT	(sizeof(int) * CHAR_BIT)
+
+LELY_UTIL_EXPORT int
+bitset_init(struct bitset *set, int size)
+{
+	assert(set);
+
+	size = MAX(1, (size + INT_BIT - 1) / INT_BIT);
+
+	unsigned int *bits = malloc(size * sizeof(int));
+	if (__unlikely(!bits))
+		return -1;
+
+	set->size = size;
+	set->bits = bits;
+
+	return 0;
+}
+
+LELY_UTIL_EXPORT void
+bitset_fini(struct bitset *set)
+{
+	assert(set);
+
+	set->size = 0;
+	free(set->bits);
+	set->bits = NULL;
+}
+
+LELY_UTIL_EXPORT int
+bitset_size(const struct bitset *set)
+{
+	return set->size * INT_BIT;
+}
+
+LELY_UTIL_EXPORT int
+bitset_resize(struct bitset *set, int size)
+{
+	assert(set);
+
+	size = MAX(1, (size + INT_BIT - 1) / INT_BIT);
+
+	unsigned int *bits = realloc(set->bits, size * sizeof(int));
+	if (__unlikely(!bits))
+		return 0;
+
+	// If the size increased, clear the new bits.
+	for (int i = set->size; i < size; i++)
+		bits[i] = 0;
+
+	set->size = size;
+	set->bits = bits;
+
+	return bitset_size(set);
+}
+
+LELY_UTIL_EXPORT int
+bitset_get_size(const struct bitset *set)
+{
+	return set->size * INT_BIT;
+}
+
+LELY_UTIL_EXPORT int
+bitset_test(const struct bitset *set, int n)
+{
+	if (__unlikely(n < 0 || n >= bitset_size(set)))
+		return 0;
+	return (set->bits[n / INT_BIT] >> (n & (INT_BIT - 1))) & 1u;
+}
+
+LELY_UTIL_EXPORT void
+bitset_set(struct bitset *set, int n)
+{
+	if (__unlikely(n < 0 || n >= bitset_size(set)))
+		return;
+	set->bits[n / INT_BIT] |= 1u << (n & (INT_BIT - 1));
+}
+
+LELY_UTIL_EXPORT void
+bitset_set_all(struct bitset *set)
+{
+	for (int i = 0; i < set->size; i++)
+		set->bits[i] = ~0u;
+}
+
+LELY_UTIL_EXPORT void
+bitset_clr(struct bitset *set, int n)
+{
+	if (__unlikely(n < 0 || n >= bitset_size(set)))
+		return;
+	set->bits[n / INT_BIT] &= ~(1u << (n & (INT_BIT - 1)));
+}
+
+LELY_UTIL_EXPORT void
+bitset_clr_all(struct bitset *set)
+{
+	for (int i = 0; i < set->size; i++)
+		set->bits[i] = 0;
+}
+
+LELY_UTIL_EXPORT void
+bitset_compl(struct bitset *set)
+{
+	for (int i = 0; i < set->size; i++)
+		set->bits[i] = ~set->bits[i];
+}
+
+LELY_UTIL_EXPORT int
+bitset_ffs(const struct bitset *set)
+{
+	const unsigned int *bits = set->bits;
+	int offset = 0;
+	for (int i = 0; i < set->size; i++) {
+		unsigned int x = *bits++;
+		if (x)
+			return offset + ffs(x);
+		offset += INT_BIT;
+	}
+	return 0;
+}
+
+LELY_UTIL_EXPORT int
+bitset_ffz(const struct bitset *set)
+{
+	const unsigned int *bits = set->bits;
+	int offset = 0;
+	for (int i = 0; i < set->size; i++) {
+		unsigned int x = *bits++;
+		if (~x)
+			return offset + ffs(~x);
+		offset += INT_BIT;
+	}
+	return 0;
+}
+
+LELY_UTIL_EXPORT int
+bitset_fns(const struct bitset *set, int n)
+{
+	if (__unlikely(n < 0))
+		n = 0;
+	if (__unlikely(n >= bitset_size(set)))
+		return 0;
+
+	int size = set->size - n / INT_BIT;
+	const unsigned int *bits = set->bits + n / INT_BIT;
+	int offset = n & ~(INT_BIT - 1);
+	n -= offset;
+	while (size) {
+		unsigned int x = *bits++;
+		if (n) {
+			// Clear the bits smaller than n.
+			x &= ~0u << n;
+			n = 0;
+		}
+		if (x)
+			return offset + ffs(x);
+		size--;
+		offset += INT_BIT;
+	}
+	return 0;
+}
+
+LELY_UTIL_EXPORT int
+bitset_fnz(const struct bitset *set, int n)
+{
+	if (__unlikely(n < 0))
+		n = 0;
+	if (__unlikely(n >= bitset_size(set)))
+		return 0;
+
+	int size = set->size - n / INT_BIT;
+	const unsigned int *bits = set->bits + n / INT_BIT;
+	int offset = n & ~(INT_BIT - 1);
+	n -= offset;
+	while (size) {
+		unsigned int x = *bits++;
+		if (n) {
+			// Set the bits smaller than n.
+			x |= ~0u >> (INT_BIT - n);
+			n = 0;
+		}
+		if (~x)
+			return offset + ffs(~x);
+		size--;
+		offset += INT_BIT;
+	}
+	return 0;
+}
+
