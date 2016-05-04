37dcba60c6746bd0647cd535bad2166cc4b6ed99
J. S. Seldenthuis <jseldenthuis@lely.com>
2016-05-04 10:22:25 +0200

    add basic library definitions

---
+++ include/lely/tap/tap.h
@@ -0,0 +1,39 @@
+/*!\file
+ * This is the public header file of the Test Anything Protocol (TAP) library.
+ *
+ * \copyright 2016 Lely Industries N.V.
+ *
+ * \author J. S. Seldenthuis <jseldenthuis@lely.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef LELY_TAP_TAP_H
+#define LELY_TAP_TAP_H
+
+#include <lely/libc/libc.h>
+
+#ifndef LELY_TAP_EXTERN
+#ifdef DLL_EXPORT
+#ifdef LELY_TAP_INTERN
+#define LELY_TAP_EXTERN	extern __dllexport
+#else
+#define LELY_TAP_EXTERN	extern __dllimport
+#endif
+#else
+#define LELY_TAP_EXTERN	extern
+#endif
+#endif
+
+#endif
+
