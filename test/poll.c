ff22fafd248109a03e5c1ff3e0f7daa9e9f9d40b
J. S. Seldenthuis <jseldenthuis@lely.com>
2016-06-30 11:15:07 +0200

    add I/O polling test

---
+++ test/poll.c
@@ -0,0 +1,62 @@
+#include <lely/tap/tap.h>
+#include <lely/io/poll.h>
+#include <lely/io/sock.h>
+
+#define TIMEOUT	1000
+
+int
+main(void)
+{
+	tap_plan(16);
+
+	io_poll_t *poll = io_poll_create();
+	tap_assert(poll);
+
+	io_handle_t pipe[2];
+	tap_test(!io_open_pipe(pipe));
+	tap_test(!io_set_flags(pipe[0], IO_FLAG_NONBLOCK));
+	tap_test(!io_set_flags(pipe[1], IO_FLAG_NONBLOCK));
+
+	struct io_event event = IO_EVENT_INIT;
+
+	int sig = 13;
+	tap_test(!io_poll_signal(poll, sig));
+	tap_test(io_poll_wait(poll, 1, &event, TIMEOUT) == 1);
+	tap_test(event.events == IO_EVENT_SIGNAL);
+	tap_test(event.u.sig == sig);
+
+	struct io_event revent = {
+		.events = IO_EVENT_READ,
+		.u.handle = pipe[0]
+	};
+	tap_assert(!io_poll_watch(poll, pipe[0], &revent, 0));
+
+	struct io_event sevent = {
+		.events = IO_EVENT_WRITE,
+		.u.handle = pipe[1]
+	};
+	tap_assert(!io_poll_watch(poll, pipe[1], &sevent, 0));
+
+	tap_test(io_poll_wait(poll, 1, &event, TIMEOUT) == 1);
+	tap_test(event.events == IO_EVENT_WRITE);
+	tap_test(event.u.handle == pipe[1]);
+
+	int sval = 13;
+	tap_test(io_write(pipe[1], &sval, sizeof(sval)) == sizeof(sval));
+
+	tap_test(io_poll_wait(poll, 1, &event, TIMEOUT) == 1);
+	tap_test(event.events == IO_EVENT_READ);
+	tap_test(event.u.handle == pipe[0]);
+
+	int rval = 0;
+	tap_test(io_read(pipe[0], &rval, sizeof(rval)) == sizeof(rval));
+
+	tap_test(rval == sval);
+
+	io_close(pipe[1]);
+	io_close(pipe[0]);
+	io_poll_destroy(poll);
+
+	return 0;
+}
+
