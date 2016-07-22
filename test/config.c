244f56a10865d7552199c09deba1ab45266bafdb
J. S. Seldenthuis <jseldenthuis@lely.com>
2016-07-22 17:17:35 +0200

    add configuration test

---
+++ test/config.c
@@ -0,0 +1,22 @@
+#include <lely/tap/tap.h>
+#include <lely/util/cmp.h>
+#include <lely/util/config.h>
+
+int
+main(void)
+{
+	tap_plan(3);
+
+	config_t *config = config_create(0);
+	tap_assert(config);
+
+	tap_test(config_parse_ini_file(config, "config.ini"));
+
+	tap_test(!str_cmp(config_get(config, "", "key"), "value"));
+	tap_test(!str_cmp(config_get(config, "section", "key"), ""));
+
+	config_destroy(config);
+
+	return 0;
+}
+
