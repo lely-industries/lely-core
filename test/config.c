7390a019e596ae34d75eb188ac190c7bdea42c52
J. S. Seldenthuis <jseldenthuis@lely.com>
2016-10-19 14:24:23 +0200

    assume TEST_SRCDIR is set

--- test/config.c
+++ test/config.c
@@ -2,10 +2,6 @@
 #include <lely/util/cmp.h>
 #include <lely/util/config.h>
 
-#ifndef TEST_SRCDIR
-#define TEST_SRCDIR
-#endif
-
 int
 main(void)
 {
@@ -14,7 +10,7 @@ main(void)
 	config_t *config = config_create(0);
 	tap_assert(config);
 
-	tap_test(config_parse_ini_file(config, TEST_SRCDIR "config.ini"));
+	tap_test(config_parse_ini_file(config, TEST_SRCDIR "/config.ini"));
 
 	tap_test(!str_cmp(config_get(config, "", "key"), "value"));
 	tap_test(!str_cmp(config_get(config, "section", "key"), ""));
