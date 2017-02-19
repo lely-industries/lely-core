9474e06e511cf82acb311da8f12ff8f62f6f6be9
J. S. Seldenthuis <jseldenthuis@lely.com>
2017-02-19 13:52:58 +0100

    add C++ compilation check

---
+++ test/cxx.cpp
@@ -0,0 +1,18 @@
+#include <lely/tap/tap.h>
+#include <lely/io/can.hpp>
+#include <lely/io/file.hpp>
+#include <lely/io/pipe.hpp>
+#include <lely/io/poll.hpp>
+#include <lely/io/serial.hpp>
+#include <lely/io/sock.hpp>
+
+using namespace lely;
+
+int
+main()
+{
+	tap_plan(0);
+
+	return 0;
+}
+
