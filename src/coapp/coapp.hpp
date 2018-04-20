df5890815de373a7288ba44a5c6dd0038f2fac00
J. S. Seldenthuis <jseldenthuis@lely.com>
2018-04-20 17:51:50 +0200

    add basic library definitions

---
+++ src/coapp/coapp.hpp
@@ -0,0 +1,35 @@
+/*!\file
+ * This is the internal header file of the C++ CANopen application library.
+ *
+ * \copyright 2018 Lely Industries N.V.
+ *
+ * \author J. S. Seldenthuis <jseldenthuis@lely.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef LELY_COAPP_INTERN_COAPP_HPP_
+#define LELY_COAPP_INTERN_COAPP_HPP_
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#define LELY_COAPP_INTERN 1
+#include <lely/coapp/coapp.hpp>
+
+#ifndef LELY_COAPP_EXPORT
+#define LELY_COAPP_EXPORT LELY_DLL_EXPORT
+#endif
+
+#endif  // LELY_COAPP_INTERN_COAPP_HPP_
