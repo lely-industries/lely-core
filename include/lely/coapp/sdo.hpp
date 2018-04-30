2c662fcfe39c4fe9bbc425474bd75555909b2011
J. S. Seldenthuis <jseldenthuis@lely.com>
2018-04-30 15:31:36 +0200

    add Client-SDO queue

---
+++ include/lely/coapp/sdo.hpp
@@ -0,0 +1,434 @@
+/*!\file
+ * This header file is part of the C++ CANopen master library; it contains the
+ * Client-SDO queue declarations.
+ *
+ * \copyright 2018 Lely Industries N.V.
+ *
+ * \author J. S. Seldenthuis <jseldenthuis@lely.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef LELY_COAPP_SDO_HPP_
+#define LELY_COAPP_SDO_HPP_
+
+#include <lely/aio/exec.hpp>
+#include <lely/coapp/detail/type_traits.hpp>
+#include <lely/coapp/sdo_error.hpp>
+
+#include <chrono>
+#include <memory>
+
+namespace lely {
+
+// The CAN network interface from <lely/can/net.hpp>.
+class CANNet;
+
+// The CANopen device from <lely/co/dev.hpp>.
+class CODev;
+
+// The CANopen Client-SDO service from <lely/co/csdo.hpp>.
+class COCSDO;
+
+namespace canopen {
+
+//! The Client-SDO queue.
+class LELY_COAPP_EXTERN Sdo {
+ public:
+  //! The type used to represent an SDO timeout duration.
+  using duration = ::std::chrono::milliseconds;
+
+ private:
+  struct Impl_;
+
+  class RequestBase_ : public aio_task {
+    friend class Sdo;
+
+   public:
+    RequestBase_(aio::ExecutorBase& exec, aio_task_func_t *func)
+        : aio_task AIO_TASK_INIT(exec, func) {}
+
+    RequestBase_(aio::ExecutorBase& exec, aio_task_func_t *func,
+                 uint16_t idx_, uint8_t subidx_, const duration& timeout_)
+        : aio_task AIO_TASK_INIT(exec, func), idx(idx_), subidx(subidx_),
+          timeout(timeout_) {}
+
+    RequestBase_(const RequestBase_&) = delete;
+
+    RequestBase_& operator=(const RequestBase_&) = delete;
+
+    virtual ~RequestBase_() = default;
+
+    aio::ExecutorBase
+    GetExecutor() const noexcept { return aio::ExecutorBase(exec); }
+
+    uint16_t idx { 0 };
+    uint8_t subidx { 0 };
+    duration timeout {};
+    uint32_t ac { 0 };
+
+   private:
+    virtual void OnRequest(Impl_* impl) noexcept = 0;
+  };
+
+  template <class T>
+  class DownloadRequestBase_ : public RequestBase_ {
+   public:
+    DownloadRequestBase_(aio::ExecutorBase& exec, aio_task_func_t *func)
+        : RequestBase_(exec, func) {}
+
+    template <class U>
+    DownloadRequestBase_(aio::ExecutorBase& exec, aio_task_func_t *func,
+        uint16_t idx, uint8_t subidx, U&& value_, const duration& timeout)
+        : RequestBase_(exec, func, idx, subidx, timeout),
+          value(::std::forward<U>(value_)) {}
+
+    T value {};
+  };
+
+  template <class T>
+  class UploadRequestBase_ : public RequestBase_ {
+    friend Impl_;
+
+   public:
+    using RequestBase_::RequestBase_;
+
+    T value {};
+  };
+
+ public:
+  /*!
+   * The signature of the callback function invoked on completion of an SDO
+   * download request. Note that the callback function SHOULD NOT throw
+   * exceptions. Since it is invoked from C, any exception that is thrown cannot
+   * be caught and will result in a call to `std::terminate()`.
+   *
+   * \param idx    the object index.
+   * \param subidx the object sub-index.
+   * \param ec     the SDO abort code (0 on success).
+   */
+  using DownloadSignature = void(uint16_t idx, uint8_t subidx,
+                                 ::std::error_code ec);
+
+  //! An SDO download request.
+  template <class T>
+  class LELY_COAPP_EXTERN DownloadRequest : public DownloadRequestBase_<T> {
+   public:
+    /*!
+     * Constructs an empty SDO download request. The object index and sub-index,
+     * the value to be written and, optionally, the SDO timeout have to be set
+     * before the request can be submitted.
+     *
+     * \param exec    the executor used to execute the confirmation function.
+     * \param con     the confirmation function to be called on completion of
+     *                the SDO request.
+     *
+     * \see Sdo::SubmitDownload<T>()
+     */
+    template <class F>
+    DownloadRequest(aio::ExecutorBase& exec, F&& con)
+        : DownloadRequestBase_<T>(exec, &Func_), con_(::std::forward<F>(con)) {}
+
+    /*!
+     * Constructs an SDO download request.
+     *
+     * \param idx     the object index.
+     * \param subidx  the object sub-index.
+     * \param value   the value to be written.
+     * \param exec    the executor used to execute the confirmation function.
+     * \param con     the confirmation function to be called on completion of
+     *                the SDO request.
+     * \param timeout the SDO timeout. If, after the request is initiated, the
+     *                timeout expires before receiving a response from the
+     *                server, the client aborts the transfer with abort code
+     *                #SdoErrc::TIMEOUT.
+     *
+     * \see Sdo::SubmitDownload<T>()
+     */
+    template <class U, class F>
+    DownloadRequest(uint16_t idx, uint8_t subidx, U&& value,
+                    aio::ExecutorBase& exec, F&& con, const duration& timeout)
+        : DownloadRequestBase_<T>(exec, &Func_, idx, subidx,
+                                  ::std::forward<U>(value), timeout),
+          con_(::std::forward<F>(con)) {}
+
+   private:
+    void OnRequest(Impl_* impl) noexcept override;
+
+    static void Func_(aio_task* task) noexcept;
+
+    ::std::function<DownloadSignature> con_ { nullptr };
+  };
+
+  /*!
+   * The signature of the callback function invoked on completion of an SDO
+   * upload request. Note that the callback function SHOULD NOT throw
+   * exceptions. Since it is invoked from C, any exception that is thrown cannot
+   * be caught and will result in a call to `std::terminate()`.
+   *
+   * \param idx    the object index.
+   * \param subidx the object sub-index.
+   * \param ec     the SDO abort code (0 on success).
+   * \param value  the value received from the SDO server.
+   */
+  template <class T>
+  using UploadSignature = void(uint16_t idx, uint8_t subidx,
+                               ::std::error_code ec, T value);
+
+  //! An SDO upload request.
+  template <class T>
+  class LELY_COAPP_EXTERN UploadRequest : public UploadRequestBase_<T> {
+   public:
+    /*!
+     * Constructs an empty SDO upload request. The object index and sub-index
+     * and, optionally, the SDO timeout have to be set before the request can be
+     * submitted.
+     *
+     * \param exec    the executor used to execute the confirmation function.
+     * \param con     the confirmation function to be called on completion of
+     *                the SDO request.
+     *
+     * \see Sdo::SubmitUpload<T>()
+     */
+    template <class F>
+    UploadRequest(aio::ExecutorBase& exec, F&& con)
+        : UploadRequestBase_<T>(exec, &Func_), con_(::std::forward<F>(con)) {}
+
+    /*!
+     * Constructs an SDO upload request.
+     *
+     * \param idx     the object index.
+     * \param subidx  the object sub-index.
+     * \param exec    the executor used to execute the confirmation function.
+     * \param con     the confirmation function to be called on completion of
+     *                the SDO request.
+     * \param timeout the SDO timeout. If, after the request is initiated, the
+     *                timeout expires before receiving a response from the
+     *                server, the client aborts the transfer with abort code
+     *                #SdoErrc::TIMEOUT.
+     *
+     * \see Sdo::SubmitUpload<T>()
+     */
+    template <class F>
+    UploadRequest(uint16_t idx, uint8_t subidx, aio::ExecutorBase& exec,
+                  F&& con, const duration& timeout)
+        : UploadRequestBase_<T>(exec, &Func_, idx, subidx, timeout),
+          con_(::std::forward<F>(con)) {}
+
+   private:
+    void OnRequest(Impl_* impl) noexcept override;
+
+    static void Func_(aio_task* task) noexcept;
+
+    ::std::function<UploadSignature<T>> con_ { nullptr };
+  };
+
+  //! Default-constructs an invalid Client-SDO queue.
+  Sdo();
+
+  /*!
+   * Constructs a Client-SDO queue for a Client-SDO from the predefined
+   * connection set (the default SDO). In general, only a CANopen master is
+   * allowed to use the default SDO.
+   *
+   * \param net a pointer to a CAN network interface (from <lely/can/net.hpp>).
+   * \param id  the node-ID of the SDO server (in the range [1..127]).
+   */
+  Sdo(CANNet* net, uint8_t id);
+
+  /*!
+   * Constructs a Client-SDO queue for a pre-configured Client-SDO. The SDO
+   * client parameter record MUST exist in the object dictionary (object 1280 to
+   * 12FF).
+   *
+   * \param net a pointer to a CAN network interface (from <lely/can/net.hpp>).
+   * \param dev a pointer to a CANopen device (from <lely/co/dev.hpp>).
+   * \param num the SDO number (in the range [1..128]).
+   */
+  Sdo(CANNet* net, CODev* dev, uint8_t num);
+
+  /*!
+   * Constructs a Client-SDO queue from an existing Client-SDO service. It is
+   * the responsibility of the caller to ensure that the SDO service remains
+   * available during the lifetime of the queue.
+   *
+   * \param sdo a pointer to a CANopen Client-SDO service (from
+                <lely/co/csdo.hpp>).
+   */
+  explicit Sdo(COCSDO* sdo);
+
+  Sdo(const Sdo&) = delete;
+  Sdo(Sdo&&) = default;
+
+  Sdo& operator=(const Sdo&) = delete;
+  Sdo& operator=(Sdo&&);
+
+  /*!
+   * Destructs the Client-SDO queue. Any ongoing or pending SDO requests are
+   * terminated with abort code #SdoErrc::DATA_CTL.
+   */
+  ~Sdo();
+
+  //! Checks whether `*this` is a valid Client-SDO queue.
+  explicit operator bool() const noexcept { return !!impl_; }
+
+  //! Queues an SDO download request.
+  template <class T>
+  typename ::std::enable_if<detail::IsCanopenType<T>::value>::type
+  SubmitDownload(DownloadRequest<T>& req) {
+    Submit(static_cast<RequestBase_&>(req));
+  }
+
+  /*!
+   * Queues an SDO download request. This function writes a value to a
+   * sub-object in a remote object dictionary.
+   *
+   * \param idx     the object index.
+   * \param subidx  the object sub-index.
+   * \param value   the value to be written.
+   * \param exec    the executor used to execute the confirmation function.
+   * \param con     the confirmation function to be called on completion of the
+   *                SDO request.
+   * \param timeout the SDO timeout. If, after the request is initiated, the
+   *                timeout expires before receiving a response from the server,
+   *                the client aborts the transfer with abort code
+   *                #SdoErrc::TIMEOUT.
+   */
+  template <class T, class F>
+  typename ::std::enable_if<
+      detail::IsCanopenType<typename ::std::decay<T>::type>::value
+  >::type
+  SubmitDownload(uint16_t idx, uint8_t subidx, T&& value,
+                 aio::ExecutorBase& exec, F&& con, const duration& timeout) {
+    using U = typename ::std::decay<T>::type;
+    auto req = new DownloadRequestWrapper<U>(idx, subidx,
+        ::std::forward<T>(value), exec, ::std::forward<F>(con), timeout);
+    Submit(*req);
+  }
+
+  /*!
+   * Aborts an SDO download request.
+   *
+   * \param req the request to be aborted.
+   * \param ac  the SDO abort code in case of an ongoing request.
+   */
+  template <class T>
+  typename ::std::enable_if<detail::IsCanopenType<T>::value,
+      ::std::size_t
+  >::type
+  CancelDownload(DownloadRequest<T>& req, SdoErrc ac) {
+    return Cancel(req, ac);
+  }
+
+  //! Queues an SDO upload request.
+  template <class T>
+  typename ::std::enable_if<detail::IsCanopenType<T>::value>::type
+  SubmitUpload(UploadRequest<T>& req) {
+    Submit(static_cast<RequestBase_&>(req));
+  }
+
+  /*!
+   * Queues an SDO upload request. This function reads the value of a sub-object
+   * in a remote object dictionary.
+   *
+   * \param idx     the object index.
+   * \param subidx  the object sub-index.
+   * \param exec    the executor used to execute the confirmation function.
+   * \param con     the confirmation function to be called on completion of the
+   *                SDO request.
+   * \param timeout the SDO timeout. If, after the request is initiated, the
+   *                timeout expires before receiving a response from the server,
+   *                the client aborts the transfer with abort code
+   *                #SdoErrc::TIMEOUT.
+   */
+  template <class T, class F>
+  typename ::std::enable_if<detail::IsCanopenType<T>::value>::type
+  SubmitUpload(uint16_t idx, uint8_t subidx, aio::ExecutorBase& exec, F&& con,
+               const duration& timeout) {
+    auto req = new UploadRequestWrapper<T>(idx, subidx, exec,
+                                           ::std::forward<F>(con), timeout);
+    Submit(*req);
+  }
+
+  /*!
+   * Aborts an SDO upload request.
+   *
+   * \param req the request to be aborted.
+   * \param ac  the SDO abort code in case of an ongoing request.
+   */
+  template <class T>
+  typename ::std::enable_if<detail::IsCanopenType<T>::value,
+      ::std::size_t
+  >::type
+  CancelUpload(UploadRequest<T>& req, SdoErrc ac) { return Cancel(req, ac); }
+
+  /*!
+   * Aborts the ongoing and all pending SDO requests.
+   *
+   * \param ac the SDO abort code.
+   */
+  ::std::size_t Cancel(SdoErrc ac);
+
+ private:
+  template <class T>
+  class LELY_COAPP_EXTERN DownloadRequestWrapper
+      : public DownloadRequestBase_<T> {
+   public:
+    template <class U, class F>
+    DownloadRequestWrapper(uint16_t idx, int8_t subidx, U&& value,
+        aio::ExecutorBase& exec, F&& con, const duration& timeout)
+        : DownloadRequestBase_<T>(exec, &Func_, idx, subidx,
+                                  ::std::forward<U>(value), timeout),
+          con_(::std::forward<F>(con)) {}
+
+   private:
+    ~DownloadRequestWrapper() = default;
+
+    void OnRequest(Impl_* impl) noexcept override;
+
+    static void Func_(aio_task* task) noexcept;
+
+    ::std::function<DownloadSignature> con_ { nullptr };
+  };
+
+  template <class T>
+  class LELY_COAPP_EXTERN UploadRequestWrapper : public UploadRequestBase_<T> {
+   public:
+    template <class F>
+    UploadRequestWrapper(uint16_t idx, uint8_t subidx, aio::ExecutorBase& exec,
+                         F&& con, const duration& timeout)
+        : UploadRequestBase_<T>(exec, &Func_, idx, subidx, timeout),
+          con_(::std::forward<F>(con)) {}
+
+   private:
+    ~UploadRequestWrapper() = default;
+
+    void OnRequest(Impl_* impl) noexcept override;
+
+    static void Func_(aio_task* task) noexcept;
+
+    ::std::function<UploadSignature<T>> con_ { nullptr };
+  };
+
+  void Submit(RequestBase_& req);
+
+  ::std::size_t Cancel(RequestBase_& req, SdoErrc ac);
+
+  ::std::unique_ptr<Impl_> impl_;
+};
+
+}  // namespace canopen
+
+}  // namespace lely
+
+#endif  // LELY_COAPP_SDO_HPP_
---
+++ src/coapp/sdo.cpp
@@ -0,0 +1,417 @@
+/*!\file
+ * This file is part of the C++ CANopen application library; it contains the
+ * implementation of the Client-SDO queue.
+ *
+ * \see lely/coapp/sdo.hpp
+ *
+ * \copyright 2018 Lely Industries N.V.
+ *
+ * \author J. S. Seldenthuis <jseldenthuis@lely.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "coapp.hpp"
+
+#include <lely/coapp/detail/chrono.hpp>
+#include <lely/coapp/sdo.hpp>
+
+#include <cassert>
+
+#include <lely/aio/queue.h>
+#include <lely/co/csdo.hpp>
+
+namespace lely {
+
+namespace canopen {
+
+namespace {
+
+::std::error_code
+ErrorCode(int errc, uint32_t ac) {
+  if (errc)
+    return ::std::error_code(errc, ::std::system_category());
+  else if (ac)
+    return static_cast<SdoErrc>(ac);
+  else
+    return {};
+}
+
+}  // namespace
+
+using namespace aio;
+
+//! The internal implementation of the Client-SDO queue.
+struct Sdo::Impl_ {
+  Impl_(CANNet* net, CODev* dev, uint8_t num);
+  Impl_(COCSDO* sdo, int timeout);
+  ~Impl_();
+
+  void Submit(RequestBase_& req);
+
+  ::std::size_t Cancel(RequestBase_* req, SdoErrc ac);
+
+  template <class T, class U>
+  void OnRequest(uint16_t idx, uint8_t subidx, U&& value);
+
+  template <class T>
+  void OnRequest(uint16_t idx, uint8_t subidx);
+
+  void OnDnCon(COCSDO*, uint16_t idx, uint8_t subidx, uint32_t ac) noexcept;
+
+  template <class T>
+  void OnUpCon(COCSDO*, uint16_t idx, uint8_t subidx, uint32_t ac, T value)
+      noexcept;
+
+  void OnTaskFinished(RequestBase_& op) noexcept;
+
+  aio_queue queue;
+  ::std::shared_ptr<COCSDO> sdo;
+};
+
+template <class T>
+LELY_COAPP_EXPORT void
+Sdo::DownloadRequest<T>::OnRequest(Impl_* impl) noexcept {
+  impl->OnRequest<T>(this->idx, this->subidx, this->value);
+}
+
+template <class T>
+LELY_COAPP_EXPORT void
+Sdo::DownloadRequest<T>::Func_(aio_task* task) noexcept {
+  auto self = static_cast<Sdo::DownloadRequest<T>*>(task);
+
+  if (self->con_)
+    self->con_(self->idx, self->subidx, ErrorCode(self->errc, self->ac));
+}
+
+template <class T>
+LELY_COAPP_EXPORT void
+Sdo::UploadRequest<T>::OnRequest(Impl_* impl) noexcept {
+  impl->OnRequest<T>(this->idx, this->subidx);
+}
+
+template <class T>
+LELY_COAPP_EXPORT void
+Sdo::UploadRequest<T>::Func_(aio_task* task) noexcept {
+  auto self = static_cast<Sdo::UploadRequest<T>*>(task);
+
+  if (self->con_)
+    self->con_(self->idx, self->subidx, ErrorCode(self->errc, self->ac),
+               ::std::move(self->value));
+}
+
+template <class T>
+LELY_COAPP_EXPORT void
+Sdo::DownloadRequestWrapper<T>::OnRequest(Impl_* impl) noexcept {
+  impl->OnRequest<T>(this->idx, this->subidx, this->value);
+}
+
+template <class T>
+LELY_COAPP_EXPORT void
+Sdo::DownloadRequestWrapper<T>::Func_(aio_task* task) noexcept {
+  auto self = static_cast<Sdo::DownloadRequestWrapper<T>*>(task);
+
+  ::std::function<DownloadSignature> con = ::std::move(self->con_);
+  auto idx = self->idx;
+  auto subidx = self->subidx;
+  auto ec = ErrorCode(self->errc, self->ac);
+  delete self;
+
+  if (con)
+    con(idx, subidx, ec);
+}
+
+template <class T>
+LELY_COAPP_EXPORT void
+Sdo::UploadRequestWrapper<T>::OnRequest(Impl_* impl) noexcept {
+  impl->OnRequest<T>(this->idx, this->subidx);
+}
+
+template <class T>
+LELY_COAPP_EXPORT void
+Sdo::UploadRequestWrapper<T>::Func_(aio_task* task) noexcept {
+  auto self = static_cast<Sdo::UploadRequestWrapper<T>*>(task);
+
+  ::std::function<UploadSignature<T>> con = ::std::move(self->con_);
+  auto idx = self->idx;
+  auto subidx = self->subidx;
+  auto ec = ErrorCode(self->errc, self->ac);
+  T value = ::std::move(self->value);
+  delete self;
+
+  if (con)
+    con(idx, subidx, ec, ::std::move(value));
+}
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+// BOOLEAN
+template class LELY_COAPP_EXPORT Sdo::DownloadRequest<bool>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequest<bool>;
+template class LELY_COAPP_EXPORT Sdo::DownloadRequestWrapper<bool>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequestWrapper<bool>;
+
+// INTEGER8
+template class LELY_COAPP_EXPORT Sdo::DownloadRequest<int8_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequest<int8_t>;
+template class LELY_COAPP_EXPORT Sdo::DownloadRequestWrapper<int8_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequestWrapper<int8_t>;
+
+// INTEGER16
+template class LELY_COAPP_EXPORT Sdo::DownloadRequest<int16_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequest<int16_t>;
+template class LELY_COAPP_EXPORT Sdo::DownloadRequestWrapper<int16_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequestWrapper<int16_t>;
+
+// INTEGER32
+template class LELY_COAPP_EXPORT Sdo::DownloadRequest<int32_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequest<int32_t>;
+template class LELY_COAPP_EXPORT Sdo::DownloadRequestWrapper<int32_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequestWrapper<int32_t>;
+
+// UNSIGNED8
+template class LELY_COAPP_EXPORT Sdo::DownloadRequest<uint8_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequest<uint8_t>;
+template class LELY_COAPP_EXPORT Sdo::DownloadRequestWrapper<uint8_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequestWrapper<uint8_t>;
+
+// UNSIGNED16
+template class LELY_COAPP_EXPORT Sdo::DownloadRequest<uint16_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequest<uint16_t>;
+template class LELY_COAPP_EXPORT Sdo::DownloadRequestWrapper<uint16_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequestWrapper<uint16_t>;
+
+// UNSIGNED32
+template class LELY_COAPP_EXPORT Sdo::DownloadRequest<uint32_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequest<uint32_t>;
+template class LELY_COAPP_EXPORT Sdo::DownloadRequestWrapper<uint32_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequestWrapper<uint32_t>;
+
+// REAL32
+template class LELY_COAPP_EXPORT Sdo::DownloadRequest<float>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequest<float>;
+template class LELY_COAPP_EXPORT Sdo::DownloadRequestWrapper<float>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequestWrapper<float>;
+
+// VISIBLE_STRING
+template class LELY_COAPP_EXPORT Sdo::DownloadRequest<::std::string>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequest<::std::string>;
+template class LELY_COAPP_EXPORT Sdo::DownloadRequestWrapper<::std::string>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequestWrapper<::std::string>;
+
+// OCTET_STRING
+template class LELY_COAPP_EXPORT
+Sdo::DownloadRequest<::std::vector<uint8_t>>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequest<::std::vector<uint8_t>>;
+template class LELY_COAPP_EXPORT
+Sdo::DownloadRequestWrapper<::std::vector<uint8_t>>;
+template class LELY_COAPP_EXPORT
+Sdo::UploadRequestWrapper<::std::vector<uint8_t>>;
+
+// UNICODE_STRING
+template class LELY_COAPP_EXPORT
+Sdo::DownloadRequest<::std::basic_string<char16_t>>;
+template class LELY_COAPP_EXPORT
+Sdo::UploadRequest<::std::basic_string<char16_t>>;
+template class LELY_COAPP_EXPORT
+Sdo::DownloadRequestWrapper<::std::basic_string<char16_t>>;
+template class LELY_COAPP_EXPORT
+Sdo::UploadRequestWrapper<::std::basic_string<char16_t>>;
+
+// TIME_OF_DAY
+// TIME_DIFFERENCE
+// DOMAIN
+// INTEGER24
+
+// REAL64
+template class LELY_COAPP_EXPORT Sdo::DownloadRequest<double>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequest<double>;
+template class LELY_COAPP_EXPORT Sdo::DownloadRequestWrapper<double>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequestWrapper<double>;
+
+// INTEGER40
+// INTEGER48
+// INTEGER56
+
+// INTEGER64
+template class LELY_COAPP_EXPORT Sdo::DownloadRequest<int64_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequest<int64_t>;
+template class LELY_COAPP_EXPORT Sdo::DownloadRequestWrapper<int64_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequestWrapper<int64_t>;
+
+// UNSIGNED24
+// UNSIGNED40
+// UNSIGNED48
+// UNSIGNED56
+
+// UNSIGNED64
+template class LELY_COAPP_EXPORT Sdo::DownloadRequest<uint64_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequest<uint64_t>;
+template class LELY_COAPP_EXPORT Sdo::DownloadRequestWrapper<uint64_t>;
+template class LELY_COAPP_EXPORT Sdo::UploadRequestWrapper<uint64_t>;
+
+#endif  // !DOXYGEN_SHOULD_SKIP_THIS
+
+LELY_COAPP_EXPORT Sdo::Sdo() = default;
+
+LELY_COAPP_EXPORT Sdo::Sdo(CANNet* net, uint8_t id) : Sdo(net, nullptr, id) {}
+
+LELY_COAPP_EXPORT
+Sdo::Sdo(CANNet* net, CODev* dev, uint8_t num)
+    : impl_(new Impl_(net, dev, num)) {}
+
+LELY_COAPP_EXPORT
+Sdo::Sdo(COCSDO* sdo) : impl_(new Impl_(sdo, sdo->getTimeout())) {}
+
+LELY_COAPP_EXPORT Sdo& Sdo::operator=(Sdo&&) = default;
+
+LELY_COAPP_EXPORT Sdo::~Sdo() = default;
+
+LELY_COAPP_EXPORT void Sdo::Submit(RequestBase_& op) { impl_->Submit(op); }
+
+LELY_COAPP_EXPORT ::std::size_t
+Sdo::Cancel(RequestBase_& op, SdoErrc ac) { return impl_->Cancel(&op, ac); }
+
+LELY_COAPP_EXPORT ::std::size_t
+Sdo::Cancel(SdoErrc ac) { return impl_->Cancel(nullptr, ac); }
+
+Sdo::Impl_::Impl_(CANNet* net, CODev* dev, uint8_t num)
+    : sdo(make_shared_c<COCSDO>(net, dev, num)) {
+  aio_queue_init(&queue);
+}
+
+Sdo::Impl_::Impl_(COCSDO* sdo_, int timeout)
+    : sdo(sdo_, [=](COCSDO* sdo) { sdo->setTimeout(timeout); }) {
+  aio_queue_init(&queue);
+}
+
+Sdo::Impl_::~Impl_() { Cancel(nullptr, SdoErrc::NO_SDO); }
+
+void
+Sdo::Impl_::Submit(RequestBase_& req) {
+  assert(req.exec);
+  req.GetExecutor().OnTaskStarted();
+  req.errc = errnum2c(ERRNUM_INPROGRESS);
+
+  if (!sdo) {
+    req.errc = 0;
+    req.ac = static_cast<uint32_t>(SdoErrc::NO_SDO);
+    OnTaskFinished(req);
+  } else {
+    bool first = aio_queue_empty(&queue);
+    aio_queue_push(&queue, &req);
+    if (first)
+      req.OnRequest(this);
+  }
+}
+
+::std::size_t
+Sdo::Impl_::Cancel(RequestBase_* req, SdoErrc ac) {
+  aio_queue queue_;
+  aio_queue_init(&queue_);
+
+  if (!req || req != aio_queue_front(&queue)) {
+    auto task = aio_queue_pop(&queue);
+    aio_queue_move(&queue_, &queue, req);
+    if (task)
+      aio_queue_push(&queue, task);
+    req = task && !req ? static_cast<RequestBase_*>(task) : nullptr;
+  }
+
+  if (req) {
+    assert(req == aio_queue_front(&queue));
+    sdo->abortReq(static_cast<uint32_t>(ac));
+  }
+
+  auto n = aio_queue_cancel(&queue_, errnum2c(ERRNUM_CANCELED));
+  if (req)
+    n += n < ::std::numeric_limits<decltype(n)>::max();
+  return n;
+}
+
+template <class T, class U>
+void
+Sdo::Impl_::OnRequest(uint16_t idx, uint8_t subidx, U&& value) {
+  constexpr auto N = co_type_traits_T<T>::index;
+
+  auto task = aio_queue_front(&queue);
+  assert(task);
+  auto& req = *static_cast<Sdo::RequestBase_*>(task);
+
+  sdo->setTimeout(detail::ToTimeout(req.timeout));
+  if (sdo->dnReq<N, Impl_, &Impl_::OnDnCon>(idx, subidx,
+      ::std::forward<U>(value), this) == -1) {
+    req.errc = get_errc();
+    OnTaskFinished(req);
+  }
+}
+
+template <class T>
+void
+Sdo::Impl_::OnRequest(uint16_t idx, uint8_t subidx) {
+  auto task = aio_queue_front(&queue);
+  assert(task);
+  auto& req = *static_cast<Sdo::RequestBase_*>(task);
+
+  sdo->setTimeout(detail::ToTimeout(req.timeout));
+  if (sdo->upReq<T, Impl_, &Impl_::OnUpCon<T>>(idx, subidx, this) == -1) {
+    req.errc = get_errc();
+    OnTaskFinished(req);
+  }
+}
+
+void
+Sdo::Impl_::OnDnCon(COCSDO*, uint16_t idx, uint8_t subidx, uint32_t ac)
+    noexcept {
+  auto task = aio_queue_pop(&queue);
+  assert(task);
+  auto& req = *static_cast<Sdo::RequestBase_*>(task);
+
+  req.errc = 0;
+  req.idx = idx;
+  req.subidx = subidx;
+  req.ac = ac;
+
+  OnTaskFinished(req);
+}
+
+template <class T>
+void
+Sdo::Impl_::OnUpCon(COCSDO*, uint16_t idx, uint8_t subidx, uint32_t ac, T value)
+    noexcept {
+  auto task = aio_queue_pop(&queue);
+  assert(task);
+  auto& req = *static_cast<Sdo::UploadRequestBase_<T>*>(task);
+
+  req.errc = 0;
+  req.idx = idx;
+  req.subidx = subidx;
+  req.ac = ac;
+  req.value = ::std::move(value);
+
+  OnTaskFinished(req);
+}
+
+void
+Sdo::Impl_::OnTaskFinished(RequestBase_& req) noexcept {
+  req.GetExecutor().Post(req);
+  req.GetExecutor().OnTaskFinished();
+
+  auto task = aio_queue_front(&queue);
+  if (task)
+    static_cast<Sdo::RequestBase_*>(task)->OnRequest(this);
+}
+
+}  // namespace canopen
+
+}  // namespace lely
