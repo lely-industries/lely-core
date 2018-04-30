9e9e08feef93e3e993320d421cc531ff00a1d1e3
J. S. Seldenthuis <jseldenthuis@lely.com>
2018-04-30 15:13:34 +0200

    add operator[] access to the local object dictionary of a slave

--- include/lely/coapp/slave.hpp
+++ include/lely/coapp/slave.hpp
@@ -52,6 +52,370 @@ class LELY_COAPP_EXTERN BasicSlave : public Node {
 
   virtual ~BasicSlave();
 
+ protected:
+  class Object;
+  class ConstObject;
+
+  /*!
+   * A mutator providing read/write access to a CANopen sub-object in a local
+   * object dictionary.
+   */
+  class SubObject {
+    friend class Object;
+
+   public:
+    SubObject(const SubObject&) = default;
+    SubObject(SubObject&&) = default;
+
+    SubObject& operator=(const SubObject&) = default;
+    SubObject& operator=(SubObject&&) = default;
+
+    /*!
+     * Sets the value of the sub-object.
+     *
+     * \param value the value to be written.
+     *
+     * \throws #lely::canopen::SdoError if the sub-object does not exist or the
+     * type does not match.
+     *
+     * \see Set()
+     */
+    template <class T>
+    SubObject&
+    operator=(T&& value) {
+      Set(::std::forward<T>(value));
+      return *this;
+    }
+
+    /*!
+     * Returns a copy of the value of the sub-object.
+     *
+     * \throws #lely::canopen::SdoError if the sub-object does not exist or the
+     * type does not match.
+     *
+     * \see Get()
+     */
+    template <class T> operator T() const { return Get<T>(); }
+
+    /*!
+     * Returns the type of the sub-object.
+     *
+     * \returns a reference to an `std::type_info` object representing the type,
+     * or `typeid(void)` if unknown.
+     *
+     * \throws #lely::canopen::SdoError if the sub-object does not exist.
+     *
+     * \see Device::Type(uint16_t idx, uint8_t subidx) const
+     */
+    const ::std::type_info&
+    Type() const { return slave_->Type(idx_, subidx_); }
+
+    /*!
+     * Returns the type of the sub-object.
+     *
+     * \param ec if the sub-object does not exist, the SDO abort code is stored
+     *           in \a ec.
+     *
+     * \returns a reference to an `std::type_info` object representing the type,
+     * or `typeid(void)` if unknown.
+     *
+     * \see Device::Type(uint16_t idx, uint8_t subidx, ::std::error_code& ec) const
+     */
+    const ::std::type_info&
+    Type(::std::error_code& ec) const {
+      return slave_->Type(idx_, subidx_, ec);
+    }
+
+    /*!
+     * Reads the value of the sub-object.
+     *
+     * \returns a copy of the value of the sub-object.
+     *
+     * \throws #lely::canopen::SdoError if the sub-object does not exist or the
+     * type does not match.
+     *
+     * \see Device::Get(uint16_t idx, uint8_t subidx) const
+     */
+    template <class T>
+    T Get() const { return slave_->Get<T>(idx_, subidx_); }
+
+    /*!
+     * Reads the value of the sub-object.
+     *
+     * \param ec if the sub-object does not exist or the type does not match,
+     *           the SDO abort code is stored in \a ec.
+     *
+     * \returns a copy of the value of the sub-object, or an empty value on
+     * error.
+     *
+     * \see Device::Get(uint16_t idx, uint8_t subidx, ::std::error_code& ec) const
+     */
+    template <class T>
+    T
+    Get(::std::error_code& ec) const {
+      return slave_->Get<T>(idx_, subidx_, ec);
+    }
+
+    /*!
+     * Writes a value to the sub-object.
+     *
+     * \param value the value to be written.
+     *
+     * \throws #lely::canopen::SdoError if the sub-object does not exist or the
+     * type does not match.
+     *
+     * \see Device::Set(uint16_t idx, uint8_t subidx, T&& value)
+     */
+    template <class T>
+    void
+    Set(T&& value) {
+      slave_->Set(idx_, subidx_, ::std::forward<T>(value));
+    }
+
+    /*!
+     * Writes a value to the sub-object.
+     *
+     * \param value the value to be written.
+     * \param ec    if the sub-object does not exist or the type does not match,
+     *              the SDO abort code is stored in \a ec.
+     *
+     * \see Device::Set(uint16_t idx, uint8_t subidx, T value, ::std::error_code& ec),
+     * Device::Set(uint16_t idx, uint8_t subidx, const T& value, ::std::error_code& ec),
+     * Device::Set(uint16_t idx, uint8_t subidx, const char* value, ::std::error_code& ec),
+     * Device::Set(uint16_t idx, uint8_t subidx, const char16_t* value, ::std::error_code& ec)
+     */
+    template <class T>
+    void
+    Set(T&& value, ::std::error_code& ec) {
+      slave_->Set(idx_, subidx_, ::std::forward<T>(value), ec);
+    }
+
+    /*!
+     * Writes an OCTET_STRING or DOMAIN value to the sub-object.
+     *
+     * \param p a pointer to the bytes to be written.
+     * \param n the number of bytes to write.
+     *
+     * \throws #lely::canopen::SdoError if the sub-object does not exist or the
+     * type does not match.
+     *
+     * \see Device::Set(uint16_t idx, uint8_t subidx, const void* p, ::std::size_t n)
+     */
+    void
+    Set(const void* p, ::std::size_t n) {
+      slave_->Set(idx_, subidx_, p, n);
+    }
+
+    /*!
+     * Writes an OCTET_STRING or DOMAIN value to the sub-object.
+     *
+     * \param p  a pointer to the bytes to be written.
+     * \param n  the number of bytes to write.
+     * \param ec if the sub-object does not exist or the type does not match,
+     *           the SDO abort code is stored in \a ac.
+     *
+     * \see Device::Set(uint16_t idx, uint8_t subidx, const void* p, ::std::size_t n, ::std::error_code& ec)
+     */
+    void
+    Set(const void* p, ::std::size_t n, ::std::error_code& ec) {
+      slave_->Set(idx_, subidx_, p, n, ec);
+    }
+
+   private:
+    SubObject(BasicSlave* slave, uint16_t idx, uint8_t subidx)
+        : slave_(slave), idx_(idx), subidx_(subidx) {}
+
+    BasicSlave* slave_;
+    uint16_t idx_;
+    uint8_t subidx_;
+  };
+
+  /*!
+   * An accessor providing read-only access to a CANopen sub-object in a local
+   * object dictionary.
+   */
+  class ConstSubObject {
+    friend class Object;
+    friend class ConstObject;
+
+   public:
+    /*!
+     * Returns a copy of the value of the sub-object.
+     *
+     * \throws #lely::canopen::SdoError if the sub-object does not exist or the
+     * type does not match.
+     *
+     * \see Get()
+     */
+    template <class T> operator T() const { return Get<T>(); }
+
+    /*!
+     * Returns the type of the sub-object.
+     *
+     * \returns a reference to an `std::type_info` object representing the type,
+     * or `typeid(void)` if unknown.
+     *
+     * \throws #lely::canopen::SdoError if the sub-object does not exist.
+     *
+     * \see Device::Type(uint16_t idx, uint8_t subidx) const
+     */
+    const ::std::type_info&
+    Type() const { return slave_->Type(idx_, subidx_); }
+
+    /*!
+     * Returns the type of the sub-object.
+     *
+     * \param ec if the sub-object does not exist, the SDO abort code is stored
+     *           in \a ec.
+     *
+     * \returns a reference to an `std::type_info` object representing the type,
+     * or `typeid(void)` if unknown.
+     *
+     * \see Device::Type(uint16_t idx, uint8_t subidx, ::std::error_code& ec) const
+     */
+    const ::std::type_info&
+    Type(::std::error_code& ec) const {
+      return slave_->Type(idx_, subidx_, ec);
+    }
+
+    /*!
+     * Reads the value of the sub-object.
+     *
+     * \returns a copy of the value of the sub-object.
+     *
+     * \throws #lely::canopen::SdoError if the sub-object does not exist or the
+     * type does not match.
+     *
+     * \see Device::Get(uint16_t idx, uint8_t subidx) const
+     */
+    template <class T>
+    T Get() const { return slave_->Get<T>(idx_, subidx_); }
+
+    /*!
+     * Reads the value of the sub-object.
+     *
+     * \param ec if the sub-object does not exist or the type does not match,
+     *           the SDO abort code is stored in \a ec.
+     *
+     * \returns a copy of the value of the sub-object, or an empty value on
+     * error.
+     *
+     * \see Device::Get(uint16_t idx, uint8_t subidx, ::std::error_code& ec) const
+     */
+    template <class T>
+    T
+    Get(::std::error_code& ec) const {
+      return slave_->Get<T>(idx_, subidx_, ec);
+    }
+
+   private:
+    ConstSubObject(const BasicSlave* slave, uint16_t idx, uint8_t subidx)
+        : slave_(slave), idx_(idx), subidx_(subidx) {}
+
+    const BasicSlave* slave_;
+    uint16_t idx_;
+    uint8_t subidx_;
+  };
+
+  /*!
+   * A mutator providing read/write access to a CANopen object in a local object
+   * dictionary.
+   */
+  class Object {
+    friend class BasicSlave;
+
+   public:
+    /*!
+     * Returns a mutator object that provides read/write access to the specified
+     * CANopen sub-object in the local object dictionary. Note that this
+     * function succeeds even if the sub-object does not exist.
+     *
+     * \param subidx the object sub-index.
+     *
+     * \returns a mutator object for a CANopen sub-object in the local object
+     * dictionary.
+     */
+    SubObject
+    operator[](uint8_t subidx) { return SubObject(slave_, idx_, subidx); }
+
+    /*!
+     * Returns an accessor object that provides read-only access to the
+     * specified CANopen sub-object in the local object dictionary. Note that
+     * this function succeeds even if the object does not exist.
+     *
+     * \param subidx the object sub-index.
+     *
+     * \returns an accessor object for a CANopen sub-object in the local object
+     * dictionary.
+     */
+    ConstSubObject
+    operator[](uint8_t subidx) const {
+      return ConstSubObject(slave_, idx_, subidx);
+    }
+
+   private:
+    Object(BasicSlave* slave, uint16_t idx) : slave_(slave), idx_(idx) {}
+
+    BasicSlave* slave_;
+    uint16_t idx_;
+  };
+
+  /*!
+   * An accessor providing read-only access to a CANopen object in a local
+   * object dictionary.
+   */
+  class ConstObject {
+    friend class BasicSlave;
+
+   public:
+    /*!
+     * Returns an accessor object that provides read-only access to the
+     * specified CANopen sub-object in the local object dictionary. Note that
+     * this function succeeds even if the object does not exist.
+     *
+     * \param subidx the object sub-index.
+     *
+     * \returns an accessor object for a CANopen sub-object in the local object
+     * dictionary.
+     */
+    ConstSubObject
+    operator[](uint8_t subidx) const {
+      return ConstSubObject(slave_, idx_, subidx);
+    }
+
+   private:
+    ConstObject(const BasicSlave* slave, uint16_t idx)
+        : slave_(slave), idx_(idx) {}
+
+    const BasicSlave* slave_;
+    uint16_t idx_;
+  };
+
+  /*!
+   * Returns a mutator object that provides read/write access to the specified
+   * CANopen object in the local object dictionary. Note that this function
+   * succeeds even if the object does not exist.
+   *
+   * \param idx the object index.
+   *
+   * \returns a mutator object for a CANopen object in the local object
+   * dictionary.
+   */
+  Object operator[](uint16_t idx) { return Object(this, idx); }
+
+  /*!
+   * Returns an accessor object that provides read-only access to the specified
+   * CANopen object in the local object dictionary. Note that this function
+   * succeeds even if the object does not exist.
+   *
+   * \param idx the object index.
+   *
+   * \returns an accessor object for a CANopen object in the local object
+   * dictionary.
+   */
+  ConstObject
+  operator[](uint16_t idx) const { return ConstObject(this, idx); }
+
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
  private:
 #endif
