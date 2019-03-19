44396a004ac4bc7a125618bbd68622163018715d
J. S. Seldenthuis <jseldenthuis@lely.com>
2019-03-19 14:39:40 +0100

    add polling event loop test

---
+++ test/ev-loop.cpp
@@ -0,0 +1,44 @@
+#include "test.h"
+#include <lely/ev/loop.hpp>
+#include <lely/ev/co_task.hpp>
+
+using namespace lely::ev;
+
+#define NUM_OP (16 * 1024 * 1024)
+
+class MyOp : public CoTask {
+ public:
+  virtual void
+  operator()() noexcept override {
+    co_reenter (*this) {
+      for (n = 0; n < NUM_OP; n++)
+        co_yield get_executor().post((ev_task&)(*this));
+    }
+  }
+
+ private:
+  ::std::size_t n;
+};
+
+int
+main() {
+  tap_plan(3);
+
+  Loop loop;
+  tap_test(!loop.stopped());
+
+  MyOp op;
+  loop.get_executor().post(op);
+
+  auto t1 = ::std::chrono::high_resolution_clock::now();
+  auto nop = loop.run();
+  auto t2 = ::std::chrono::high_resolution_clock::now();
+
+  tap_test(nop == NUM_OP + 1);
+  tap_test(loop.stopped());
+
+  auto ns = ::std::chrono::nanoseconds(t2 - t1).count();
+  tap_diag("%f ns per op", double(ns) / nop);
+
+  return 0;
+}
