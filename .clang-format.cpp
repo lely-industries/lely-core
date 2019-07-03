45d8e5eaae89ace57b241a9598a12fc4b1aec0d0
J. S. Seldenthuis <jseldenthuis@lely.com>
2019-07-03 13:48:10 +0200

    add clang-format configuration for C++

---
+++ .clang-format.cpp
@@ -0,0 +1,12 @@
+---
+Language: Cpp
+BasedOnStyle: Google
+AlignEscapedNewlines: DontAlign
+AlwaysBreakAfterReturnType: AllDefinitions
+DerivePointerAlignment: false
+ForEachMacros:
+  - co_reenter
+PointerAlignment: Left
+SortIncludes: false
+Standard: Cpp11
+...
