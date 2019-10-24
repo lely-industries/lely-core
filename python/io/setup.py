5c1eb350c952a946cb64d0a69fcbb01fff8772e2
J. S. Seldenthuis <jseldenthuis@lely.com>
2019-10-24 10:37:33 +0200

    release v2.0.1

--- python/io/setup.py
+++ python/io/setup.py
@@ -31,7 +31,7 @@ if USE_CYTHON:
 
 setup(
     name='lely_io',
-    version='1.9.2',
+    version='2.0.1',
     description='Python bindings for the Lely I/O library.',
     url='https://gitlab.com/lely_industries/lely-core',
     author='J. S. Seldenthuis',
