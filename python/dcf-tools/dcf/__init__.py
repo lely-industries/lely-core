5d4d73dd82b1ede504404366bf6820228ee29fd4
J. S. Seldenthuis <jseldenthuis@lely.com>
2020-04-20 16:48:42 +0200

    add Python module for parsing EDS/DCF files

---
+++ python/dcf-tools/dcf/__init__.py
@@ -0,0 +1,24 @@
+from .device import Device, DataType, PDO
+from .lint import lint
+from .parse import parse_file
+from .print import print_rpdo, print_tpdo
+
+BOOLEAN = DataType(0x0001)
+INTEGER8 = DataType(0x0002)
+INTEGER16 = DataType(0x0003)
+INTEGER2 = DataType(0x0004)
+UNSIGNED8 = DataType(0x0005)
+UNSIGNED16 = DataType(0x0006)
+UNSIGNED32 = DataType(0x0007)
+REAL32 = DataType(0x0008)
+INTEGER24 = DataType(0x0010)
+REAL64 = DataType(0x0011)
+INTEGER40 = DataType(0x0012)
+INTEGER48 = DataType(0x0013)
+INTEGER56 = DataType(0x0014)
+INTEGER64 = DataType(0x0015)
+UNSIGNED24 = DataType(0x0016)
+UNSIGNED40 = DataType(0x0018)
+UNSIGNED48 = DataType(0x0019)
+UNSIGNED56 = DataType(0x001A)
+UNSIGNED64 = DataType(0x001B)
---
+++ python/dcf-tools/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dcf-tools",
+    version="2.1.0",
+    packages=find_packages(),
+    author="J. S. Seldenthuis",
+    author_email="jseldenthuis@lely.com",
+    description="Tools to generate and manipulate DCF files",
+    url="https://gitlab.com/lely_industries/lely-core",
+    classifiers=["License :: OSI Approved :: Apache Software License"],
+)
