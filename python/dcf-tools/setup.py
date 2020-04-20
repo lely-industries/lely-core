bdd9c96cafd06ebd49691211e4a634cb4ca6a26b
J. S. Seldenthuis <jseldenthuis@lely.com>
2020-04-20 16:50:56 +0200

    add dcfchk tool

--- python/dcf-tools/setup.py
+++ python/dcf-tools/setup.py
@@ -4,6 +4,7 @@ setup(
     name="dcf-tools",
     version="2.1.0",
     packages=find_packages(),
+    entry_points={"console_scripts": ["dcfchk = dcf.cli:main"]},
     author="J. S. Seldenthuis",
     author_email="jseldenthuis@lely.com",
     description="Tools to generate and manipulate DCF files",
