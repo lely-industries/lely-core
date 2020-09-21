b0c3e7ec7656b0eb33bee45e40fef2bf64abfa3c
Konrad Grochowski <kgrochowski@n7space.com>
2020-09-21 12:50:09 +0200

    release v3.0.0

--- python/can/setup.py
+++ python/can/setup.py
@@ -31,7 +31,7 @@ if USE_CYTHON:
 
 setup(
     name='lely_can',
-    version='2.3.0',
+    version='3.0.0',
     description='Python bindings for the Lely CAN library.',
     url='https://gitlab.com/lely_industries/lely-core',
     author='J. S. Seldenthuis',
