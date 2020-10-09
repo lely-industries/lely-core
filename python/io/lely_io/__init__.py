0db71442046818f1c4a78e1d9e052402d76c2dbb
J. S. Seldenthuis <jseldenthuis@lely.com>
2020-10-09 15:03:28 +0200

    release v2.2.3

--- python/io/lely_io/__init__.py
+++ python/io/lely_io/__init__.py
@@ -2,7 +2,7 @@
 
 __copyright__ = '2020 Lely Industries N.V.'
 __author__ = 'J. S. Seldenthuis <jseldenthuis@lely.com>'
-__version__ = '2.2.2'
+__version__ = '2.2.3'
 __license__ = 'Apache-2.0'
 
 from lely_io.addr import *
