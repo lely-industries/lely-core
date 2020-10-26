5a28d0394b66e53a280effef70714091866e8c75
Konrad Grochowski <kgrochowski@n7space.com>
2020-10-26 14:47:53 +0100

    post-release commit

--- python/dcf-tools/setup.py
+++ python/dcf-tools/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dcf-tools",
-    version="3.0.1",
+    version="3.0.2",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
