ae0527a01c1cc8ed0f202dcf6f88bfc6dcc50311
J. S. Seldenthuis <jseldenthuis@lely.com>
2020-11-16 17:49:50 +0100

    release v2.2.5

--- python/dcf-tools/setup.py
+++ python/dcf-tools/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dcf-tools",
-    version="2.2.1",
+    version="2.2.5",
     packages=find_packages(),
     entry_points={
         "console_scripts": ["dcfchk = dcf.cli:main", "dcfgen = dcfgen.cli:main"]
