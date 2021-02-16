7e6269868d5c01381ae0739ec715f7def4e5592b
Wojciech Cierpucha <wcierpucha@n7space.com>
2021-02-16 19:55:06 +0100

    test-mempool.cpp: added GWT test documentation + one test case.

--- unit-tests/util/test-mempool.cpp
+++ unit-tests/util/test-mempool.cpp
@@ -1,7 +1,7 @@
 /**@file
  * This file is part of the CANopen Library Unit Test Suite.
  *
- * @copyright 2020 N7 Space Sp. z o.o.
+ * @copyright 2020-2021 N7 Space Sp. z o.o.
  *
  * Unit Test Suite was developed under a programme of,
  * and funded by, the European Space Agency.
@@ -31,6 +31,36 @@
 #include <lely/util/mempool.h>
 #include <lely/util/error.h>
 
+TEST_GROUP(Util_MemPoolInit) {};
+
+/// @name mempool_init()
+///@{
+
+/// \Given an uninitialized memory pool and a memory buffer
+/// \When mempool_init() is called with a pointer to the pool, the memory buffer
+///       and its size
+/// \Then a pointer to an abstract allocator (alloc_t) is returned with non-null
+///       function pointers, memory pool is initialized with the memory buffer
+///       and points at it beginning
+TEST(Util_MemPoolInit, MemPool_Init) {
+  mempool pool;
+  const size_t size = 10;
+  char buffer[size] = {0};
+
+  const auto* alloc = mempool_init(&pool, buffer, size);
+
+  CHECK((*alloc)->alloc != nullptr);
+  CHECK((*alloc)->free != nullptr);
+  CHECK((*alloc)->size != nullptr);
+  CHECK((*alloc)->capacity != nullptr);
+
+  POINTERS_EQUAL(pool.beg, buffer);
+  POINTERS_EQUAL(pool.end, buffer + size);
+  POINTERS_EQUAL(pool.cur, buffer);
+}
+
+///@}
+
 TEST_GROUP(Util_MemPool) {
   static const size_t POOL_SIZE = 1024u;
   mempool pool;
@@ -40,12 +70,37 @@ TEST_GROUP(Util_MemPool) {
   TEST_SETUP() { alloc = mempool_init(&pool, memory, POOL_SIZE); }
 };
 
+/// @name mem_size()
+///@{
+
+/// \Given a pointer to an allocator (alloc_t) based on memory pool, with no
+///        prior allocations
+/// \When mem_size() is called
+/// \Then zero is returned
 TEST(Util_MemPool, MemPool_Size) { CHECK_EQUAL(0, mem_size(alloc)); }
 
+///@}
+
+/// @name mem_capacity()
+///@{
+
+/// \Given a pointer to an allocator (alloc_t) based on memory pool
+/// \When mem_capacity() is called
+/// \Then total memory pool size is returned
 TEST(Util_MemPool, MemPool_Capacity) {
   CHECK_EQUAL(POOL_SIZE, mem_capacity(alloc));
 }
 
+///@}
+
+/// @name mem_alloc()
+
+/// \Given a pointer to an allocator (alloc_t) based on memory pool, with no
+///        prior allocations
+/// \When mem_alloc() is called with zero alignment and non-zero allocation size
+/// \Then a pointer pointing to memory pool's buffer first byte is returned;
+///       mem_size() returns requested allocation size, remaining capacity
+///       returned by mem_capacity() is lower by requested size
 TEST(Util_MemPool, MemPool_Alloc) {
   const size_t allocationSize = 10u;
 
@@ -56,6 +111,15 @@ TEST(Util_MemPool, MemPool_Alloc) {
   CHECK_EQUAL(POOL_SIZE - allocationSize, mem_capacity(alloc));
 }
 
+/// \Given a pointer to an allocator (alloc_t) based on memory pool, with no
+///        prior allocations
+/// \When mem_alloc() is called twice with non-zero alignment and non-zero size
+///       that's not a multiple of alignment's value
+/// \Then both calls return non-null pointers that point into memory pool's
+///       buffer and are apart by requested allocation size rounded up to a
+///       multiple of requested alignment; mem_size() returns twice the
+///       requested allocation size plus required padding, remaining capacity is
+///       lowered by same value
 TEST(Util_MemPool, MemPool_Alloc_RespectsAlignment) {
   const size_t allocationSize = 11u;
   const size_t alignment = 2u;
@@ -69,12 +133,18 @@ TEST(Util_MemPool, MemPool_Alloc_RespectsAlignment) {
   CHECK_EQUAL(POOL_SIZE - mem_size(alloc), mem_capacity(alloc));
 }
 
+/// \Given a pointer to an allocator (alloc_t) based on memory pool, with no
+///        prior allocations
+/// \When mem_alloc() is called with non-zero alignment that's not a power of 2
+///       and non-zero size
+/// \Then a null pointer is returned, nothing is changed;
+///       if !LELY_NO_ERRNO invalid argument error is reported
 TEST(Util_MemPool, MemPool_Alloc_IncorrectAlignment) {
   const size_t allocationSize = 10u;
 
   const auto result = mem_alloc(alloc, 3u, allocationSize);
 
-  POINTERS_EQUAL(NULL, result);
+  POINTERS_EQUAL(nullptr, result);
   CHECK_EQUAL(0, mem_size(alloc));
   CHECK_EQUAL(POOL_SIZE, mem_capacity(alloc));
 #if !LELY_NO_ERRNO
@@ -82,12 +152,18 @@ TEST(Util_MemPool, MemPool_Alloc_IncorrectAlignment) {
 #endif
 }
 
+/// \Given a pointer to an allocator (alloc_t) based on memory pool, with no
+///        prior allocations
+/// \When mem_alloc() is called with zero alignment and size that's greater than
+///       memory pool's buffer size
+/// \Then a null pointer is returned, nothing is changed;
+///       if !LELY_NO_ERRNO no memory error is reported
 TEST(Util_MemPool, MemPool_Alloc_OutOfMemory) {
   const size_t allocationSize = POOL_SIZE + 1u;
 
   const auto result = mem_alloc(alloc, 0, allocationSize);
 
-  POINTERS_EQUAL(NULL, result);
+  POINTERS_EQUAL(nullptr, result);
   CHECK_EQUAL(0, mem_size(alloc));
   CHECK_EQUAL(POOL_SIZE, mem_capacity(alloc));
 #if !LELY_NO_ERRNO
@@ -95,17 +171,30 @@ TEST(Util_MemPool, MemPool_Alloc_OutOfMemory) {
 #endif
 }
 
+/// \Given a pointer to an allocator (alloc_t) based on memory pool, with no
+///        prior allocations
+/// \When mem_alloc() is called with zero alignment and zero size
+/// \Then a null pointer is returned, nothing is changed;
+///       no error is reported
 TEST(Util_MemPool, MemPool_Alloc_SizeZero) {
   set_errnum(0);
 
   const auto result = mem_alloc(alloc, 0, 0);
 
-  POINTERS_EQUAL(NULL, result);
+  POINTERS_EQUAL(nullptr, result);
   CHECK_EQUAL(0, mem_size(alloc));
   CHECK_EQUAL(POOL_SIZE, mem_capacity(alloc));
   CHECK_EQUAL(0, get_errnum());
 }
 
+///@}
+
+/// @name mem_free()
+
+/// \Given pointers to an allocator (alloc_t) based on memory pool and some
+///        allocated memory
+/// \When mem_free() is called with a pointer to allocated memory
+/// \Then nothing is changed
 TEST(Util_MemPool, MemPool_Free_DoeaNothing) {
   const size_t allocationSize = 10u;
   const auto result = mem_alloc(alloc, 0, allocationSize);
@@ -116,3 +205,5 @@ TEST(Util_MemPool, MemPool_Free_DoeaNothing) {
   CHECK_EQUAL(allocationSize, mem_size(alloc));
   CHECK_EQUAL(POOL_SIZE - allocationSize, mem_capacity(alloc));
 }
+
+///@}
