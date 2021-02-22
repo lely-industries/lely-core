ad012dd04957d27ceb0f291d874be508fe31fd7c
Wojciech Cierpucha <wcierpucha@n7space.com>
2021-02-22 13:42:13 +0000

    test-sync.cpp: create sync in fixture setup.
    It was required by all test cases in fixture anyway.

--- unit-tests/co/test-sync-sdo.cpp
+++ unit-tests/co/test-sync-sdo.cpp
@@ -1,7 +1,7 @@
 /**@file
  * This file is part of the CANopen Library Unit Test Suite.
  *
- * @copyright 2020 N7 Space Sp. z o.o.
+ * @copyright 2020-2021 N7 Space Sp. z o.o.
  *
  * Unit Test Suite was developed under a programme of,
  * and funded by, the European Space Agency.
@@ -112,9 +112,18 @@ TEST_GROUP(CO_SyncSdo) {
   }
 };
 
-// given: valid SYNC
-// when: co_1005_dn_ind()
-// then: CO_SDO_AC_TYPE_LEN_LO abort code is returned
+/// @name SYNC service: object 0x1005 modification using SDO
+///@{
+
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1005 with
+///        COB-ID SYNC value set
+///
+/// \When a value shorter than 4 bytes is downloaded to object 0x1005 using SDO
+///
+/// \Then CO_SDO_AC_TYPE_LEN_LO abort code is passed to CSDO download
+///       confirmation function, nothing is changed
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
 TEST(CO_SyncSdo, Co1005Dn_TypeLenTooLow) {
   const co_unsigned8_t num_of_mappings = 1u;
   const auto ret =
@@ -126,9 +135,17 @@ TEST(CO_SyncSdo, Co1005Dn_TypeLenTooLow) {
   CHECK_EQUAL(CO_SDO_AC_TYPE_LEN_LO, CoCsdoDnCon::ac);
 }
 
-// given: valid SYNC
-// when: co_1005_dn_ind()
-// then: CO_SDO_AC_NO_SUB abort code is returned
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1005 with
+///        COB-ID SYNC set and with an additional sub-object at some non-zero
+///        sub-index set to some value
+///
+/// \When the sub-object is changed using SDO
+///
+/// \Then CO_SDO_AC_NO_SUB abort code is passed to CSDO download confirmation
+///       function, nothing is changed
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
 TEST(CO_SyncSdo, Co1005Dn_InvalidSubobject) {
   obj1005->InsertAndSetSub(0x01u, CO_DEFTYPE_UNSIGNED32,
                            co_unsigned32_t(DEV_ID));
@@ -144,9 +161,16 @@ TEST(CO_SyncSdo, Co1005Dn_InvalidSubobject) {
   CHECK_EQUAL(CO_SDO_AC_NO_SUB, CoCsdoDnCon::ac);
 }
 
-// given: valid SYNC
-// when: co_1005_dn_ind()
-// then: 0 abort code is returned
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1005 with
+///        COB-ID SYNC set
+///
+/// \When object 0x1005 is modified using SDO with same value as already set
+///
+/// \Then 0 is returned, nothing is changed
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
+///       \Calls co_sub_get_val_u32()
 TEST(CO_SyncSdo, Co1005Dn_SameAsPrevious) {
   const co_unsigned32_t cobid = DEV_ID;
   const auto ret =
@@ -158,9 +182,18 @@ TEST(CO_SyncSdo, Co1005Dn_SameAsPrevious) {
   CHECK_EQUAL(0, CoCsdoDnCon::ac);
 }
 
-// given: valid SYNC
-// when: co_1005_dn_ind()
-// then: CO_SDO_AC_PARAM_VAL abort code is returned
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1005 with
+///        COB-ID SYNC with producer bit set
+///
+/// \When object 0x1005 is modified using SDO with new COB-ID SYNC with producer
+///       bit set
+///
+/// \Then CO_SDO_AC_PARAM_VAL abort code is passed to CSDO download confirmation
+///       function, nothing is changed
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
+///       \Calls co_sub_get_val_u32()
 TEST(CO_SyncSdo, Co1005Dn_ProducerToProducer_NewCanId) {
   SetCobid(DEV_ID | CO_SYNC_COBID_PRODUCER);
 
@@ -174,9 +207,17 @@ TEST(CO_SyncSdo, Co1005Dn_ProducerToProducer_NewCanId) {
   CHECK_EQUAL(CO_SDO_AC_PARAM_VAL, CoCsdoDnCon::ac);
 }
 
-// given: valid SYNC
-// when: new COB-ID with the same CAN-ID is downloaded
-// then: 0 abort code is returned
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1005 with
+///        COB-ID SYNC with producer bit set
+///
+/// \When object 0x1005 is modified using SDO to new COB-ID with the same CAN-ID
+///
+/// \Then 0 is returned, new COB-ID is set in the 0x1005 object
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
+///       \Calls co_sub_get_val_u32()
+///       \Calls co_sub_dn()
 TEST(CO_SyncSdo, Co1005Dn_ProducerToProducer_SameCanId_NewCobid) {
   SetCobid(DEV_ID | CO_SYNC_COBID_PRODUCER);
 
@@ -189,11 +230,22 @@ TEST(CO_SyncSdo, Co1005Dn_ProducerToProducer_SameCanId_NewCobid) {
   CHECK_EQUAL(0, ret);
   CHECK(CoCsdoDnCon::called());
   CHECK_EQUAL(0, CoCsdoDnCon::ac);
+
+  CHECK_EQUAL(cobid, co_obj_get_val_u32(obj1005->Get(), 0x00u));
 }
 
-// given: valid SYNC
-// when: co_1005_dn_ind()
-// then: 0 abort code is returned
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1005 with
+///        COB-ID SYNC set
+///
+/// \When object 0x1005 is modified using SDO to new COB-ID with the same CAN-ID
+///       but with producer bit set
+///
+/// \Then 0 is returned, new COB-ID is set in the 0x1005 object
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
+///       \Calls co_sub_get_val_u32()
+///       \Calls co_sub_dn()
 TEST(CO_SyncSdo, Co1005Dn_ConsumerToProducer_SameCanId) {
   const co_unsigned32_t cobid = DEV_ID | CO_SYNC_COBID_PRODUCER;
   const auto ret =
@@ -203,11 +255,22 @@ TEST(CO_SyncSdo, Co1005Dn_ConsumerToProducer_SameCanId) {
   CHECK_EQUAL(0, ret);
   CHECK(CoCsdoDnCon::called());
   CHECK_EQUAL(0, CoCsdoDnCon::ac);
+
+  CHECK_EQUAL(cobid, co_obj_get_val_u32(obj1005->Get(), 0x00u));
 }
 
-// given: valid SYNC
-// when: co_1005_dn_ind()
-// then: CO_SDO_AC_PARAM_VAL abort code is returned
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1005 with
+///        COB-ID SYNC set
+///
+/// \When object 0x1005 is modified using SDO to new COB-ID using Extended
+///       Identifier but without frame bit set
+///
+/// \Then CO_SDO_AC_PARAM_VAL abort code is passed to CSDO download confirmation
+///       function, nothing is changed
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
+///       \Calls co_sub_get_val_u32()
 TEST(CO_SyncSdo, Co1005Dn_ExtendedId_NoFrameBit) {
   const co_unsigned32_t cobid = DEV_ID | (1u << 28u);
   const auto ret =
@@ -219,9 +282,18 @@ TEST(CO_SyncSdo, Co1005Dn_ExtendedId_NoFrameBit) {
   CHECK_EQUAL(CO_SDO_AC_PARAM_VAL, CoCsdoDnCon::ac);
 }
 
-// given: valid SYNC
-// when: co_1005_dn_ind()
-// then: 0 abort code is returned
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1005 with
+///        COB-ID SYNC set
+///
+/// \When object 0x1005 is modified using SDO to new COB-ID using Extended
+///       Identifier with frame bit set
+///
+/// \Then 0 is returned, new COB-ID is set in the 0x1005 object
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
+///       \Calls co_sub_get_val_u32()
+///       \Calls co_sub_dn()
 TEST(CO_SyncSdo, Co1005Dn_FrameBit) {
   const co_unsigned32_t cobid = DEV_ID | CO_SYNC_COBID_FRAME;
   const auto ret =
@@ -231,11 +303,24 @@ TEST(CO_SyncSdo, Co1005Dn_FrameBit) {
   CHECK_EQUAL(0, ret);
   CHECK(CoCsdoDnCon::called());
   CHECK_EQUAL(0, CoCsdoDnCon::ac);
+
+  CHECK_EQUAL(cobid, co_obj_get_val_u32(obj1005->Get(), 0x00u));
 }
 
-// given: valid SYNC
-// when: co_1006_dn_ind()
-// then: CO_SDO_AC_TYPE_LEN_LO abort code is returned
+///@}
+
+/// @name SYNC service: object 0x1006 modification using SDO
+///@{
+
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1006
+///        with communication cycle period set to 0
+///
+/// \When a value shorter than 4 bytes is downloaded to object 0x1006 using SDO
+///
+/// \Then CO_SDO_AC_TYPE_LEN_LO abort code is passed to CSDO download
+///       confirmation function, nothing is changed
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
 TEST(CO_SyncSdo, Co1006Dn_TypeLenTooLow) {
   const co_unsigned16_t period = 0;
   const auto ret =
@@ -247,9 +332,17 @@ TEST(CO_SyncSdo, Co1006Dn_TypeLenTooLow) {
   CHECK_EQUAL(CO_SDO_AC_TYPE_LEN_LO, CoCsdoDnCon::ac);
 }
 
-// given: valid SYNC
-// when: co_1006_dn_ind()
-// then: CO_SDO_AC_NO_SUB abort code is returned
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1006 with
+///        communication cycle period set to 0 and with an additional sub-object
+///        at some non-zero sub-index set to some value
+///
+/// \When the sub-object is changed using SDO
+///
+/// \Then CO_SDO_AC_NO_SUB abort code is passed to CSDO download confirmation
+///       function, nothing is changed
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
 TEST(CO_SyncSdo, Co1006Dn_InvalidSubobject) {
   obj1006->InsertAndSetSub(0x01u, CO_DEFTYPE_UNSIGNED16,
                            co_unsigned16_t(0x00u));
@@ -265,9 +358,16 @@ TEST(CO_SyncSdo, Co1006Dn_InvalidSubobject) {
   CHECK_EQUAL(CO_SDO_AC_NO_SUB, CoCsdoDnCon::ac);
 }
 
-// given: valid SYNC
-// when: co_1006_dn_ind()
-// then: 0 abort code is returned
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1006
+///        with communication cycle period set to 0
+///
+/// \When object 0x1006 is modified using SDO with same value as already set
+///
+/// \Then 0 is returned, nothing is changed
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
+///       \Calls co_sub_get_val_u32()
 TEST(CO_SyncSdo, Co1006Dn_SameAsPrevious) {
   const co_unsigned32_t period = 0;
   const auto ret =
@@ -279,10 +379,19 @@ TEST(CO_SyncSdo, Co1006Dn_SameAsPrevious) {
   CHECK_EQUAL(0, CoCsdoDnCon::ac);
 }
 
-// given: valid SYNC
-// when: co_1006_dn_ind()
-// then: 0 abort code is returned
-TEST(CO_SyncSdo, Co1006Dn) {
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1006 with
+///        communication cycle period set to 0
+///
+/// \When object 0x1006 is modified using SDO with a new value
+///
+/// \Then 0 is returned, communication cycle period is set to the new value in
+///       the 0x1006 object
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
+///       \Calls co_sub_get_val_u32()
+///       \Calls co_sub_dn()
+TEST(CO_SyncSdo, Co1006Dn_Nominal) {
   const co_unsigned32_t period = 231u;
   const auto ret =
       co_dev_dn_val_req(dev, 0x1006, 0x00u, CO_DEFTYPE_UNSIGNED32, &period,
@@ -291,11 +400,24 @@ TEST(CO_SyncSdo, Co1006Dn) {
   CHECK_EQUAL(0, ret);
   CHECK(CoCsdoDnCon::called());
   CHECK_EQUAL(0, CoCsdoDnCon::ac);
+
+  CHECK_EQUAL(period, co_obj_get_val_u32(obj1006->Get(), 0x00u));
 }
 
-// given: valid SYNC
-// when: co_1019_dn_ind()
-// then: CO_SDO_AC_TYPE_LEN_HI abort code is returned
+///@}
+
+/// @name SYNC service: object 0x1019 modification using SDO
+///@{
+
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1019 with
+///        counter overflow value set to 0
+///
+/// \When a value larger than one byte is downloaded to object 0x1019 using SDO
+///
+/// \Then CO_SDO_AC_TYPE_LEN_HI abort code is passed to CSDO download
+///       confirmation function, nothing is changed
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
 TEST(CO_SyncSdo, Co1019Dn_TypeLenTooHigh) {
   const co_unsigned16_t data = 0u;
   const auto ret =
@@ -307,9 +429,17 @@ TEST(CO_SyncSdo, Co1019Dn_TypeLenTooHigh) {
   CHECK_EQUAL(CO_SDO_AC_TYPE_LEN_HI, CoCsdoDnCon::ac);
 }
 
-// given: valid SYNC
-// when: co_1019_dn_ind()
-// then: CO_SDO_AC_NO_SUB abort code is returned
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1019 with
+///        counter overflow value set to 0 and with an additional sub-object at
+///        some non-zero sub-index set to some value
+///
+/// \When the sub-object is changed using SDO
+///
+/// \Then CO_SDO_AC_NO_SUB abort code is passed to CSDO download confirmation
+///       function, nothing is changed
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
 TEST(CO_SyncSdo, Co1019Dn_InvalidSubobject) {
   obj1019->InsertAndSetSub(0x01u, CO_DEFTYPE_UNSIGNED8, co_unsigned8_t(0x00u));
   RestartSYNC();
@@ -324,9 +454,16 @@ TEST(CO_SyncSdo, Co1019Dn_InvalidSubobject) {
   CHECK_EQUAL(CO_SDO_AC_NO_SUB, CoCsdoDnCon::ac);
 }
 
-// given: valid SYNC
-// when: co_1019_dn_ind()
-// then: 0 abort code is returned
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1019 with
+///        counter overflow value set to 0
+///
+/// \When object 0x1019 is modified using SDO with same value as already set
+///
+/// \Then 0 is returned, nothing is changed
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
+///       \Calls co_sub_get_val_u8()
 TEST(CO_SyncSdo, Co1019Dn_SameAsPrevious) {
   const co_unsigned8_t cnt = 0u;
   const auto ret = co_dev_dn_val_req(dev, 0x1019u, 0x00u, CO_DEFTYPE_UNSIGNED8,
@@ -337,9 +474,18 @@ TEST(CO_SyncSdo, Co1019Dn_SameAsPrevious) {
   CHECK_EQUAL(0, CoCsdoDnCon::ac);
 }
 
-// given: valid SYNC
-// when: co_1019_dn_ind()
-// then: CO_SDO_AC_DATA_DEV abort code is returned
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1019 with
+///        counter overflow value set to 0 and object 0x1006 with communication
+///        cycle period set to a non-zero value
+///
+/// \When object 0x1019 is modified using SDO with a new value
+///
+/// \Then CO_SDO_AC_DATA_DEV abort code is passed to CSDO download confirmation
+///       function, nothing is changed
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
+///       \Calls co_sub_get_val_u8()
 TEST(CO_SyncSdo, Co1019Dn_CommCyclePeriodNotZero) {
   co_sub_t* const sub = co_dev_find_sub(dev, 0x1006u, 0x00u);
   co_sub_set_val_u32(sub, CO_UNSIGNED32_MAX);
@@ -354,9 +500,18 @@ TEST(CO_SyncSdo, Co1019Dn_CommCyclePeriodNotZero) {
   CHECK_EQUAL(CO_SDO_AC_DATA_DEV, CoCsdoDnCon::ac);
 }
 
-// given: valid SYNC
-// when: co_1019_dn_ind()
-// then: CO_SDO_AC_PARAM_VAL abort code is returned
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1019 with
+///        counter overflow value set to 0
+///
+/// \When object 0x1019 is modified using SDO with new value equal to the lower
+///       limit
+///
+/// \Then CO_SDO_AC_PARAM_VAL abort code is passed to CSDO download confirmation
+///       function, nothing is changed
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
+///       \Calls co_sub_get_val_u8()
 TEST(CO_SyncSdo, Co1019Dn_OverflowEveryTime) {
   const co_unsigned8_t cnt = 1u;
   const auto ret = co_dev_dn_val_req(dev, 0x1019u, 0x00u, CO_DEFTYPE_UNSIGNED8,
@@ -367,9 +522,18 @@ TEST(CO_SyncSdo, Co1019Dn_OverflowEveryTime) {
   CHECK_EQUAL(CO_SDO_AC_PARAM_VAL, CoCsdoDnCon::ac);
 }
 
-// given: valid SYNC
-// when: co_1019_dn_ind()
-// then: CO_SDO_AC_PARAM_VAL abort code is returned
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1019 with
+///        counter overflow value set to 0
+///
+/// \When object 0x1019 is modified using SDO with new value greater than the
+///       maximum allowed counter overflow value
+///
+/// \Then CO_SDO_AC_PARAM_VAL abort code is passed to CSDO download confirmation
+///       function, nothing is changed
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
+///       \Calls co_sub_get_val_u8()
 TEST(CO_SyncSdo, Co1019Dn_OverflowMoreThanMax) {
   const co_unsigned8_t cnt = 241u;
   const auto ret = co_dev_dn_val_req(dev, 0x1019u, 0x00u, CO_DEFTYPE_UNSIGNED8,
@@ -380,10 +544,20 @@ TEST(CO_SyncSdo, Co1019Dn_OverflowMoreThanMax) {
   CHECK_EQUAL(CO_SDO_AC_PARAM_VAL, CoCsdoDnCon::ac);
 }
 
-// given: valid SYNC
-// when: co_1019_dn_ind()
-// then: 0 abort code is returned
-TEST(CO_SyncSdo, Co1019Dn) {
+/// \Given a pointer to started SYNC service (co_sync_t), object 0x1019 with
+///        counter overflow value set to 0
+///
+/// \When object 0x1019 is modified using SDO with a new value within allowed
+///       limits
+///
+/// \Then 0 is returned, counter overflow value is set to new value in the
+///       0x1019 object
+///       \Calls co_sub_get_type()
+///       \Calls co_sdo_req_dn_val()
+///       \Calls co_sub_get_subidx()
+///       \Calls co_sub_get_val_u8()
+///       \Calls co_sub_dn()
+TEST(CO_SyncSdo, Co1019Dn_Nominal) {
   const co_unsigned8_t cnt = 32u;
   const auto ret = co_dev_dn_val_req(dev, 0x1019u, 0x00u, CO_DEFTYPE_UNSIGNED8,
                                      &cnt, nullptr, CoCsdoDnCon::func, nullptr);
@@ -391,4 +565,8 @@ TEST(CO_SyncSdo, Co1019Dn) {
   CHECK_EQUAL(0, ret);
   CHECK(CoCsdoDnCon::called());
   CHECK_EQUAL(0, CoCsdoDnCon::ac);
+
+  CHECK_EQUAL(cnt, co_obj_get_val_u8(obj1019->Get(), 0x00u));
 }
+
+///@}
