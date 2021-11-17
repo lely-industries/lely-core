d528a7c06ed10a3b10ef1ea5a87cebcb23e12fbf
Konrad Grochowski <kgrochowski@n7space.com>
2021-11-17 17:43:54 +0100

    post v3.1.3 release version update

--- python/dcf-tools/setup.py
+++ python/dcf-tools/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dcf-tools",
-    version="3.1.3",
+    version="3.1.4",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
